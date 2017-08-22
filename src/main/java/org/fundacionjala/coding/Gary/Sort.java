4b141ab9a46b20dc802f979d0f48e52db0cd74c1
GaryFuentes <gary.fuentes@fundacion-jala.org>
2017-08-22 08:57:29 -0400

    kjd

--- src/main/java/org/fundacionjala/coding/Gary/Sort.java
+++ src/main/java/org/fundacionjala/coding/Gary/Sort.java
@@ -1,26 +1,38 @@
+package org.fundacionjala.coding.Gary;
+
 import java.util.Arrays;
 
 /**
+ * test.
  * Created by Gary on 17/8/2017.
  */
 public class Sort {
     private String chain;
     private String space;
-    public Sort(String chain){
-        this.chain=chain;
-        space="[ ]+";
-    };
 
-    public String HighAndLow()
-    {
-        String[] chain1= chain.split(space);
-        int[] numbers= new int[chain1.length];
-        for(int i=0; i< chain1.length;i++)
-        {
-            numbers[i]=Integer.parseInt(chain1[i]);
+    /**
+     * test.
+     * @param chain test.
+     *              test.
+     */
+    public Sort(String chain) {
+        this.chain = chain;
+        space = "[ ]+";
+    }
+
+
+    /**
+     *test.
+     * @return test.
+     */
+    public String highAndLow() {
+        String[] chain1 = chain.split(space);
+        int[] numbers = new int[chain1.length];
+        for (int i = 0; i < chain1.length; i++) {
+            numbers[i] = Integer.parseInt(chain1[i]);
         }
         Arrays.sort(numbers);
-        return numbers[numbers.length-1]+" "+numbers[0];
+        return numbers[numbers.length - 1] + " " + numbers[0];
 
     }
 }
--- src/test/java/org/fundacionjala/coding/Gary/TestSort.java
+++ src/test/java/org/fundacionjala/coding/Gary/TestSort.java
@@ -1,4 +1,5 @@
-import com.company.Main;
+package org.fundacionjala.coding.Gary;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,24 +7,35 @@ import org.junit.Test;
  * Created by Gary on 17/8/2017.
  */
 
-public class testSort {
+/**
+ *  test.
+ */
+public class TestSort {
+    /**
+     * test.
+     */
     @Test
     public void testFirstCase() {
         Sort sort = new Sort("1 2 3 4 5");
-        Assert.assertEquals("5 1", sort.HighAndLow());
+        Assert.assertEquals("5 1", sort.highAndLow());
     }
 
+    /**
+     * test.
+     */
     @Test
-    public void testSecondCase()
-    {
+    public void testSecondCase() {
         Sort sort = new Sort("1 2 -3 4 5");
-        Assert.assertEquals("5 -3", sort.HighAndLow());
+        Assert.assertEquals("5 -3", sort.highAndLow());
     }
+
+    /**
+     * test.
+     */
     @Test
-    public void testThirdCase()
-    {
+    public void testThirdCase() {
         Sort sort = new Sort("1 9 3 4 -5");
-        Assert.assertEquals("9 -5", sort.HighAndLow());
+        Assert.assertEquals("9 -5", sort.highAndLow());
 
     }
 }
