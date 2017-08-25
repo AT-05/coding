941027fdc5c9dc8b838863397fbe1822c7ada790
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-08-25 09:12:13 +0000

    Fixed code review observation

--- src/main/java/org/fundacionjala/coding/franco/HighAndLow.java
+++ src/main/java/org/fundacionjala/coding/franco/HighAndLow.java
@@ -7,19 +7,20 @@ import java.util.Arrays;
  */
 public class HighAndLow {
     /**
+     * Gets high and low numbers.
+     *
      * @param numbers Is a string that contains a set of numbers separated by a space.
      * @return Highest and Lowest numbers of the set.
      */
     public String highAndLowNumbers(String numbers) {
-        int first, last;
         String[] arrayNumbers = numbers.split("\\s+");
         int[] arrayOfIntNumbers = new int[arrayNumbers.length];
         for (int i = 0; i < arrayNumbers.length; i++) {
             arrayOfIntNumbers[i] = Integer.parseInt(arrayNumbers[i]);
         }
         Arrays.sort(arrayOfIntNumbers);
-        first = arrayOfIntNumbers[0];
-        last = arrayOfIntNumbers[arrayOfIntNumbers.length - 1];
+        int first = arrayOfIntNumbers[0];
+        int last = arrayOfIntNumbers[arrayOfIntNumbers.length - 1];
 
         return String.format("%s %s", last, first);
     }
