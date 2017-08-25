dacaa9cc868bfd6e024f71bce263d86524ffe7ee
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-08-25 09:03:39 +0000

    Adding Bank OCR kata3

--- src/main/java/org/fundacionjala/coding/Franco/AccountNumber.java
+++ src/main/java/org/fundacionjala/coding/Franco/AccountNumber.java
@@ -1,7 +1,63 @@
 package org.fundacionjala.coding.Franco;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 8/25/2017.
  */
 public class AccountNumber {
+    private static final int NUMBER_SIZE = 3;
+    private static final List<String> ACCOUNT_NUMBERS = new ArrayList<>();
+    static {
+        ACCOUNT_NUMBERS.add(" _ " + "| |" + "|_|");
+        ACCOUNT_NUMBERS.add("   " + "  |" + "  |");
+        ACCOUNT_NUMBERS.add(" _ " + " _|" + "|_ ");
+        ACCOUNT_NUMBERS.add(" _ " + " _|" + " _|");
+        ACCOUNT_NUMBERS.add("   " + "|_|" + "  |");
+        ACCOUNT_NUMBERS.add(" _ " + "|_ " + " _|");
+        ACCOUNT_NUMBERS.add(" _ " + "|_ " + "|_|");
+        ACCOUNT_NUMBERS.add(" _ " + "  |" + "  |");
+        ACCOUNT_NUMBERS.add(" _ " + "|_|" + "|_|");
+        ACCOUNT_NUMBERS.add(" _ " + "|_|" + " _|");
+    }
+
+    public int getDigit(String number) {
+        for (int i = 0; i < ACCOUNT_NUMBERS.size(); i++) {
+            if (ACCOUNT_NUMBERS.get(i).equals(number)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String mapEntryOCRToNumber(List<String> lines) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < lines.get(0).length(); i += NUMBER_SIZE) {
+            String number = lines.get(0).substring(i, i + NUMBER_SIZE)
+                    .concat(lines.get(1).substring(i, i + NUMBER_SIZE))
+                    .concat(lines.get(2).substring(i, i + NUMBER_SIZE));
+            result.append(getDigit(number));
+        }
+        return result.toString();
+    }
+
+    public List<String> parseFile(String fileName) throws IOException {
+        BufferedReader fileBuffer = null;
+        FileReader fileReader = new FileReader(fileName);
+        fileBuffer = new BufferedReader(fileReader);
+        String line = null;
+        ArrayList<String> lines = new ArrayList<String>();
+
+        while ((line = fileBuffer.readLine()) != null) {
+            if (!line.equals("")) {
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
 }
--- src/test/java/org/fundacionjala/coding/Franco/AccountNumberTest.java
+++ src/test/java/org/fundacionjala/coding/Franco/AccountNumberTest.java
@@ -1,8 +1,41 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.Franco;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 8/25/2017.
  */
 public class AccountNumberTest {
+    @Test
+    public void testMapEntryOCRToNumber(){
+        List<String> lines = new ArrayList<>();
+        lines.add("    _  _     _  _  _  _  _ ");
+        lines.add("  | _| _||_||_ |_   ||_||_|");
+        lines.add("  ||_  _|  | _||_|  ||_| _|");
 
+        AccountNumber accountNumber1 = new AccountNumber();
+
+        final String actualResult = accountNumber1.mapEntryOCRToNumber(lines);
+
+        final String expectedResult = "123456789";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testParsedAccountNumber() throws IOException {
+        String fileName = "C:\\Users\\Administrator\\Desktop\\AccountNumbers.txt";
+        ArrayList<String> lines = new ArrayList<String>();
+
+        AccountNumber accountNumber1 = new AccountNumber();
+
+        String result = accountNumber1.mapEntryOCRToNumber(accountNumber1.parseFile(fileName));
+
+        assertEquals("490067715", result);
+    }
 }
