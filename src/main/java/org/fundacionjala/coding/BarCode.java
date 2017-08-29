f08853dee1f5fadfe2887ef616a76e2212f1134b
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-08-29 11:50:02 -0400

    Abner Kata 6

--- src/main/java/org/fundacionjala/coding/BarCode.java
+++ src/main/java/org/fundacionjala/coding/BarCode.java
@@ -1,7 +1,35 @@
-package org.fundacionjala.coding.Abner;
+package org.fundacionjala.coding;
 
 /**
+ * this class check de code bar.
  * Created by Administrator on 8/29/2017.
  */
 public class BarCode {
+
+    /**
+     * This function check is the code bar is true.
+     * @param codBar number de code bal.
+     * @return boolean.
+     */
+    public boolean checkCodeBar(String codBar) {
+        int checkSum = Integer.parseInt(codBar.substring(codBar.length() - 1));
+        int sum = 0;
+        for (int i = 1; i < codBar.length() - 1; i++) {
+            if (i % 2 == 0) {
+                sum += (int) (codBar.charAt(i));
+            } else {
+                sum += ((int) (codBar.charAt(i)) * 3);
+            }
+        }
+
+        if (sum % 10 == 0) {
+            return (checkSum == 0) ? true : false;
+
+        } else {
+            return ((10 - (sum % 10)) == checkSum) ? true : false;
+
+        }
+
+
+    }
 }
--- src/test/java/org/fundacionjala/coding/Abner/TestBarCode.java
+++ src/test/java/org/fundacionjala/coding/Abner/TestBarCode.java
@@ -1,7 +1,42 @@
 package org.fundacionjala.coding.Abner;
 
+import org.fundacionjala.coding.BarCode;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
- * Created by Administrator on 8/29/2017.
+ * Created by Abner on 8/29/2017.
  */
 public class TestBarCode {
+    /**
+     * The var to all class.
+     */
+    private BarCode barCode;
+
+    /**
+     * Setup de var for all class.
+     */
+    @Before
+    public void setup() {
+        barCode = new BarCode();
+    }
+
+    /**
+     * The function testing the number  4003301018398.
+     */
+    @Test
+    public void testChecksumTrue() {
+        assertEquals(true, barCode.checkCodeBar("4003301018398"));
+    }
+
+    /**
+     * The function testing the number  4003301018398.
+     */
+    @Test
+    public void testChecksumFalse() {
+        assertEquals(false, barCode.checkCodeBar("4003301018392"));
+    }
+
 }
