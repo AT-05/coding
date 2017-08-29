4c93b87f3e213fb57a4f96af5f0c52a4ca2dbd53
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-08-29 11:49:14 +0000

    Adding kata 6

--- src/main/java/org/fundacionjala/coding/Franco/EANValidation.java
+++ src/main/java/org/fundacionjala/coding/Franco/EANValidation.java
@@ -1,7 +1,24 @@
 package org.fundacionjala.coding.Franco;
 
+import java.util.stream.Stream;
+
 /**
  * Created by Administrator on 8/29/2017.
  */
 public class EANValidation {
+    /**
+     *
+     * @param chainOfNumbers is an European Article Number.
+     * @return if the checksum of the first twelve numbers of this EAN number
+     * is the same as its last digit.
+     */
+    public boolean checksumValidation(String chainOfNumbers) {
+        final int[] numbers = Stream.of(chainOfNumbers.split("")).mapToInt(Integer::parseInt).toArray();
+        int totalSum = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            totalSum += ((i + 1) % 2 == 0) ? (numbers[i] * 3) : (numbers[i]);
+        }
+        int checkSum = (totalSum % 10 == 0) ? 0 : 10 - (totalSum % 10);
+        return numbers[numbers.length - 1] == checkSum;
+    }
 }
--- src/test/java/org/fundacionjala/coding/Franco/EANValidationTest.java
+++ src/test/java/org/fundacionjala/coding/Franco/EANValidationTest.java
@@ -1,8 +1,24 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.Franco;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Administrator on 8/29/2017.
  */
 public class EANValidationTest {
+    /**
+     * This method test outputs of checksumValidation method.
+     */
+    @Test
+    public void testChecksumValidation() {
+        EANValidation eanValidation = new EANValidation();
 
+        final boolean actualResult1 = eanValidation.checksumValidation("4003301018392");
+        assertFalse(actualResult1);
+
+        final boolean actualResult2 = eanValidation.checksumValidation("4003301018398");
+        assertTrue(actualResult2);
+    }
 }
