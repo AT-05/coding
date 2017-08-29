3743558b3e1c006d2b936505b86ffe2739186b4c
marcomendez <marco.mendez@fundacion-jala.org>
2017-08-29 19:35:07 +0000

    Adding the exercise  kata5

--- src/main/java/org/fundacionjala/coding/Marco/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/Marco/SortInnerContent.java
@@ -1,4 +1,38 @@
 package org.fundacionjala.coding.Marco;
 
+import java.util.Arrays;
+
+/**
+ * This kata was made by marco mendez august 29, 2017.
+ */
 public class SortInnerContent {
+
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+
+    /**
+     * This method do the convect of a string.
+     *
+     * @param string test.
+     * @return test.
+     */
+    public String sortInnerContent(String string) {
+        String[] world = string.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String value : world) {
+            if (value.length() == ONE) {
+                result.append(value);
+                result.append(" ");
+            } else {
+                result.append(value.substring(ZERO, ONE));
+                String[] aux = value.substring(1, value.length() - ONE).split("");
+                Arrays.sort(aux);
+                result.append((new StringBuffer(string.join("", aux)).reverse().toString()));
+                result.append(value.substring(value.length() - ONE, value.length()));
+                result.append(" ");
+            }
+
+        }
+        return result.toString().trim();
+    }
 }
--- src/test/java/org/fundacionjala/coding/Marco/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/Marco/SortInnerContentTest.java
@@ -1,4 +1,58 @@
 package org.fundacionjala.coding.Marco;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This kata was made by marco mendez august 29, 2017.
+ */
 public class SortInnerContentTest {
+    /**
+     * Test example 1.
+     */
+    @Test
+    public void testSortInnerContent1() {
+        SortInnerContent sort = new SortInnerContent();
+        String expectResult = "srot the inner ctonnet in dsnnieedcg oredr";
+        String actualResult = "sort the inner content in descending order";
+        assertEquals(expectResult, sort.sortInnerContent(actualResult));
+
+    }
+
+    /**
+     * Test example 2.
+     */
+    @Test
+    public void testSortInnerContent2() {
+        SortInnerContent sort = new SortInnerContent();
+        String expectResult = "wiat for me";
+        String actualResult = "wait for me";
+        assertEquals(expectResult, sort.sortInnerContent(actualResult));
+
+    }
+
+    /**
+     * Test example 3.
+     */
+    @Test
+    public void testSortInnerContent3() {
+        SortInnerContent sort = new SortInnerContent();
+        String expectResult = "tihs ktaa is esay";
+        String actualResult = "this kata is easy";
+        assertEquals(expectResult, sort.sortInnerContent(actualResult));
+
+    }
+
+    /**
+     * Test example 4.
+     */
+    @Test
+    public void testSortInnerContent4() {
+        SortInnerContent sort = new SortInnerContent();
+        String expectResult = "I am";
+        String actualResult = "I am";
+        assertEquals(expectResult, sort.sortInnerContent(actualResult));
+
+    }
 }
