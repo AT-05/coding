e747073267baa51ff8370ce6848238e2ca098b56
atge <german.quinonez@fundacion-jala.com>
2017-08-29 15:42:26 -0400

    update kata 4

--- src/main/java/org/fundacionjala/coding/german/SpinWords.java
+++ src/main/java/org/fundacionjala/coding/german/SpinWords.java
@@ -4,4 +4,26 @@ package org.fundacionjala.coding.german;
  * Created by Administrator on 8/29/2017.
  */
 public class SpinWords {
+    /**
+     * @param words String
+     * @return String
+     */
+    public String spinWords(String words) {
+        String delimiter = " ";
+        String[] temp = words.split(delimiter);
+        StringBuilder temBuilder = new StringBuilder();
+        for (String s : temp) {
+            if (s.length() >= 5) {
+                temBuilder.append(new StringBuilder(s).reverse().toString());
+
+            } else {
+                temBuilder.append(s);
+            }
+            temBuilder.append(" ");
+
+        }
+        temBuilder.deleteCharAt(temBuilder.length() - 1);
+        return temBuilder.toString();
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/german/SpinWordTest.java
+++ src/test/java/org/fundacionjala/coding/german/SpinWordTest.java
@@ -1,7 +1,58 @@
 package org.fundacionjala.coding.german;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 /**
  * Created by Administrator on 8/29/2017.
  */
 public class SpinWordTest {
+    /**
+     * Verify spinWords method.
+     */
+    @Test
+    public void testSpinWordMethodTwoReverseWords() {
+        SpinWords spinWord = new SpinWords();
+        assertEquals("Hey wollef sroirraw", spinWord.spinWords("Hey fellow warriors"));
+    }
+
+    /**
+     * Verify spinWords method.
+     */
+    @Test
+    public void testSpinWordMethodZeroReveseWord() {
+        SpinWords spinWord = new SpinWords();
+        assertEquals("This is a test", spinWord.spinWords("This is a test"));
+    }
+
+    /**
+     * Verify spinWords method.
+     */
+    @Test
+    public void testSpinWordMethodOneReveseWord() {
+        SpinWords spinWord = new SpinWords();
+        assertEquals("This is rehtona test", spinWord.spinWords("This is another test"));
+    }
+
+    /**
+     * Verify spinWords method.
+     */
+    @Test
+    public void testSpinWordMethodOnlyOneNotReveseWord() {
+        SpinWords spinWord = new SpinWords();
+        assertEquals("word", spinWord.spinWords("word"));
+    }
+
+    /**
+     * Verify spinWords method.
+     */
+    @Test
+    public void testSpinWordMethodOnlyOneReveseWord() {
+        SpinWords spinWord = new SpinWords();
+        assertEquals("sdrow", spinWord.spinWords("words"));
+    }
+
+
 }
