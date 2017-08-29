e59da5e7b1acf9e57ccd14a45ed38e9c2b8a072f
atge <german.quinonez@fundacion-jala.com>
2017-08-29 11:21:52 -0400

    german kata 6

--- src/main/java/org/fundacionjala/coding/german/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/german/EANValidator.java
@@ -4,4 +4,51 @@ package org.fundacionjala.coding.german;
  * Created by Administrator on 8/29/2017.
  */
 public class EANValidator {
+    /**
+     * @param number String
+     * @return boolean
+     */
+    public boolean validate(String number) {
+        boolean res = false;
+        int sumPar = sumPar(number);
+        int sumImpar = sumImpar(number);
+        int checkSum = sumImpar + sumPar;
+        if ((checkSum) % 10 == 0) {
+            if (number.charAt(12) == '0') {
+                res = true;
+            }
+
+        } else {
+            if ((10 - (checkSum % 10) == ((int) number.charAt(12) - 48))) {
+                res = true;
+            } else {
+                res = false;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * @param number String
+     * @return int
+     */
+    private int sumImpar(String number) {
+        int res = 0;
+        for (int i = 0; i < number.length() - 1; i += 2) {
+            res = res + ((int) number.charAt(i) - 48);
+        }
+        return res;
+    }
+
+    /**
+     * @param number String
+     * @return int
+     */
+    private int sumPar(String number) {
+        int res = 0;
+        for (int i = 1; i < number.length() - 1; i += 2) {
+            res = res + ((int) number.charAt(i) - 48) * 3;
+        }
+        return res;
+    }
 }
--- src/test/java/org/fundacionjala/coding/german/EANValidatorTest.java
+++ src/test/java/org/fundacionjala/coding/german/EANValidatorTest.java
@@ -1,7 +1,34 @@
 package org.fundacionjala.coding.german;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Administrator on 8/29/2017.
  */
 public class EANValidatorTest {
+
+    /**
+     * Verify validate method.
+     */
+    @Test
+    public void testValidateMethodIsTrue() {
+        EANValidator eanValidatorTest = new EANValidator();
+        boolean result = eanValidatorTest.validate("4003301018398");
+        assertTrue("validate Method should return 'true'", result);
+    }
+
+    /**
+     * Verify validate method.
+     */
+    @Test
+    public void testValidateMethodIsFalse() {
+        EANValidator eanValidatorTest = new EANValidator();
+        boolean result = eanValidatorTest.validate("4003301018392");
+        assertFalse("validate Method should return 'true'", result);
+    }
+
 }
