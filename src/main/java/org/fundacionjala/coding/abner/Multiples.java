3144b82621551e0e9696c3f1e90b3fb27c0db874
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-08-29 15:05:53 -0400

    Abner Fix Kata 1 and 2

--- src/main/java/org/fundacionjala/coding/abner/Multiples.java
+++ src/main/java/org/fundacionjala/coding/abner/Multiples.java
@@ -5,25 +5,19 @@ package org.fundacionjala.coding.abner;
  */
 public class Multiples {
 
-    private int mult1, mult2;
+    private static final int MULTIPLE_THREE = 3;
+    private static final int MULTIPLE_FIVE = 5;
 
     /**
-     *Initializing values.
-     */
-    public Multiples() {
-        mult1 = 3;
-        mult2 = 5;
-    }
-
-    /**
-     *
-     * @param numMax  Multiplier numbers.
+     * @param numMax Multiplier numbers.
      * @return the sum total.
      */
+
+
     public int multiplesSum(int numMax) {
         int sum = 0;
         for (int i = 1; i < numMax; i++) {
-            if (i % mult1 == 0 || i % mult2 == 0) {
+            if (i % MULTIPLE_THREE == 0 || i % MULTIPLE_FIVE == 0) {
                 sum += i;
             }
         }
