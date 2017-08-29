5d3e02a08badcc165fc8331f6faaa5f804971225
marcomendez <marco.mendez@fundacion-jala.org>
2017-08-29 16:00:22 +0000

    adding kata6

--- src/main/java/org/fundacionjala/coding/marco/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/marco/EANValidator.java
@@ -1,4 +1,31 @@
 package org.fundacionjala.coding.marco;
+import java.util.stream.Stream;
 
+/**
+ * This was made by Marco Mendez august 29, 2017.
+ */
 public class EANValidator {
+
+
+    public static final int ZERO = 0;
+    public static final int ONO = 1;
+    public static final int THREE = 3;
+    public static final int TEN = 10;
+
+    /**
+     * This method return checkSum validated.
+     *
+     * @param valor string.
+     * @return boolean.
+     */
+    public boolean validator(String valor) {
+        int[] vector = Stream.of(valor.split("")).mapToInt(Integer::parseInt).toArray();
+        int sum = ZERO;
+        for (int i = ZERO; i < vector.length - ONO; i++) {
+            sum += (i + 1) % 2 == 0 ? vector[i] * THREE : vector[i];
+        }
+        int checkSum = (sum % TEN) == ZERO ? ZERO : (TEN - (sum % TEN));
+        return (checkSum == vector[vector.length - ONO]);
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/marco/EANValidatorTest.java
+++ src/test/java/org/fundacionjala/coding/marco/EANValidatorTest.java
@@ -1,4 +1,25 @@
 package org.fundacionjala.coding.marco;
 
-public class EANValidator {
+import org.junit.Test;
+
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * this is my test.
+ */
+public class EANValidatorTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void testEAN() {
+        EANValidator validator = new EANValidator();
+        assertTrue(validator.validator("4003301018398")); // True
+        assertFalse(validator.validator("4003301018392")); // False
+
+    }
+
+
 }
