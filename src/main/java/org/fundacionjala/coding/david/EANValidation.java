50a56c2766b8a1e09363082336d325dacc78308e
maizman <david.maizman@fundacion-org.com>
2017-08-29 16:16:14 +0000

    Add Kata 6 write for David Maizman

--- src/main/java/org/fundacionjala/coding/david/EANValidation.java
+++ src/main/java/org/fundacionjala/coding/david/EANValidation.java
@@ -1,7 +1,28 @@
 package org.fundacionjala.coding.david;
 
+import java.util.stream.Stream;
+
 /**
- * Created by Administrator on 8/29/2017.
+ * This class realize check number.
  */
 public class EANValidation {
+
+    private static final int NUMBER_ONE = 1;
+    private static final int NUMBER_THREE = 3;
+
+    /**
+     * This method realize check sum .
+     * @param number is number.
+     * @return true or false.
+     */
+    public boolean checkSum(String number) {
+        int[] listNumber = Stream.of(number.split("")).mapToInt(Integer::parseInt).toArray();
+        int resultTotal = 0;
+        for (int i = 0; i < listNumber.length - 1; i++) {
+            resultTotal += ((i + 1) % 2 == 0) ? listNumber[i] * NUMBER_THREE : listNumber[i] * NUMBER_ONE;
+        }
+        int checkSum = (resultTotal % 10 == 0) ? 0 : 10 - (resultTotal % 10);
+        int lastNumber = listNumber[12];
+        return lastNumber == checkSum;
+    }
 }
--- src/test/java/org/fundacionjala/coding/david/EANValidationTest.java
+++ src/test/java/org/fundacionjala/coding/david/EANValidationTest.java
@@ -1,7 +1,25 @@
 package org.fundacionjala.coding.david;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Administrator on 8/29/2017.
+ * This class is for test check number.
  */
 public class EANValidationTest {
+
+    /**
+     * this method realize test of number.
+     */
+    @Test
+    public void checkSumTest() {
+       EANValidation eanValidation = new EANValidation();
+       final boolean actualResult = eanValidation.checkSum("4003301018398");
+       assertTrue(actualResult);
+
+       final boolean actualResult2 = eanValidation.checkSum("4003301018392");
+       assertFalse(actualResult2);
+
+    }
 }
