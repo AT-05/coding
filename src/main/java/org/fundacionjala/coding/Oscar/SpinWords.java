5d54ce1733ba007814ac634191210a9dd7361f49
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-08-29 23:52:59 -0400

    Adding spinword kata 4

--- src/main/java/org/fundacionjala/coding/Oscar/SpinWords.java
+++ src/main/java/org/fundacionjala/coding/Oscar/SpinWords.java
@@ -4,4 +4,20 @@ package org.fundacionjala.coding.Oscar;
  * Created by Oz64 on 29/08/2017.
  */
 public class SpinWords {
+    /**
+     * This method checks when the length is greater or equal to 5 in a word, that word is reversed.
+     *
+     * @param stringWords the string with the words
+     * @return the string with the word reversed if is necessary.
+     */
+    public String spindWord(String stringWords) {
+        String[] words = stringWords.split(" ");
+        for (int index = 0; index < words.length; index++) {
+            if (words[index].length() >= 5) {
+                StringBuilder builder = new StringBuilder(words[index]);
+                words[index] = builder.reverse().toString();
+            }
+        }
+        return String.join(" ", words);
+    }
 }
--- src/test/java/org/fundacionjala/coding/Oscar/SpinWordsTest.java
+++ src/test/java/org/fundacionjala/coding/Oscar/SpinWordsTest.java
@@ -1,7 +1,48 @@
 package org.fundacionjala.coding.Oscar;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Oz64 on 29/08/2017.
  */
 public class SpinWordsTest {
+    private SpinWords kata4;
+
+    /**
+     * This method execute before any unit test.
+     */
+    @Before
+    public void setup() {
+        kata4 = new SpinWords();
+    }
+
+    /**
+     * This test verifies in a string of words, reversed two.
+     */
+    @Test
+    public void testSpinTwoWords() {
+        String expectResult = kata4.spindWord("Hey fellow warriors");
+        assertEquals("Hey wollef sroirraw", expectResult);
+    }
+
+    /**
+     * This test verifies in a string of words, reversed one.
+     */
+    @Test
+    public void testSpinOneWord() {
+        String expectResult = kata4.spindWord("This is another test");
+        assertEquals("This is rehtona test", expectResult);
+    }
+
+    /**
+     * This test verifies in a string of words, reversed none.
+     */
+    @Test
+    public void testSpinNoneWord() {
+        String expectResult = kata4.spindWord("This is a test");
+        assertEquals("This is a test", expectResult);
+    }
 }
