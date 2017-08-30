1e63618f91ac5818fabb1c8890fcadc84c3ac0ab
GaryFN <gary.fuentes66@gmail.com>
2017-08-29 23:53:00 -0400

    add kata4

--- src/main/java/org/fundacionjala/coding/Gary/Spin.java
+++ src/main/java/org/fundacionjala/coding/Gary/Spin.java
@@ -4,4 +4,26 @@ package org.fundacionjala.coding.Gary;
  * Created by Gary on 29/8/2017.
  */
 public class Spin {
+    private static final String DELIMITER = " ";
+    private String[] separateWords;
+
+    /**
+     * the method that converts.
+     * @param chain to convert.
+     * @return converted chain.
+     */
+    public String spinWords(String chain) {
+        separateWords = new String[]{};
+        separateWords = chain.split(DELIMITER);
+        StringBuilder builder;
+        for (int position = 0; position < separateWords.length; position++) {
+
+            if (separateWords[position].length() >= 5) {
+                builder = new StringBuilder(separateWords[position]);
+                separateWords[position] = builder.reverse().toString();
+            }
+        }
+
+        return String.join(DELIMITER, separateWords);
+    }
 }
--- src/test/java/org/fundacionjala/coding/Gary/TestSpin.java
+++ src/test/java/org/fundacionjala/coding/Gary/TestSpin.java
@@ -1,7 +1,61 @@
 package org.fundacionjala.coding.Gary;
 
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Gary on 29/8/2017.
  */
 public class TestSpin {
+    private Spin spin;
+
+    /**
+     * setup.
+     */
+    @Before
+    public void setup() {
+        spin = new Spin();
+    }
+
+
+    /**
+     * test first case.
+     */
+    @Test
+    public void testSpindWordsCaseOne() {
+
+        String expectedResult = "Hey wollef sroirraw";
+        String actualResult = spin.spinWords("Hey fellow warriors");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * test second case.
+     */
+    @Test
+    public void testSpindWordsSecondCase() {
+
+        String expectedResult = "This is a test";
+        String actualResult = spin.spinWords("This is a test");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * test third case.
+     */
+    @Test
+    public void testSpindWordsThirdCase() {
+
+        String expectedResult = "This is rehtona test";
+        String actualResult = spin.spinWords("This is another test");
+        assertEquals(expectedResult, actualResult);
+    }
+
+
 }
+
+
+
