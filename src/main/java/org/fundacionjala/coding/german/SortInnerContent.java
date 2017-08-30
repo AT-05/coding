a347f676833304492ad9ea628650446b64032dbd
atge <german.quinonez@fundacion-jala.com>
2017-08-30 15:36:48 -0400

    adding kata 5

--- src/main/java/org/fundacionjala/coding/german/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/german/SortInnerContent.java
@@ -1,7 +1,50 @@
 package org.fundacionjala.coding.german;
 
+import java.util.Arrays;
+
+
 /**
- * Created by Administrator on 8/29/2017.
+ * Created by German on 8/29/2017.
  */
 public class SortInnerContent {
+    /**
+     * @param words String
+     * @return String  words in descending order and only inner
+     */
+    public String sortInnerConntent(String words) {
+        String delimiter = " ";
+        String[] temp = words.split(delimiter);
+        StringBuilder temBuilder = new StringBuilder();
+        for (String s : temp) {
+            temBuilder.append(descendingOrderInner(s));
+            temBuilder.append(" ");
+
+        }
+        temBuilder.deleteCharAt(temBuilder.length() - 1);
+        return temBuilder.toString();
+    }
+
+    /**
+     * @param word String
+     * @return String  in descending order and only inner
+     */
+    private String descendingOrderInner(String word) {
+
+        if (word.length() < 4) {
+            return word;
+        } else {
+            String subString = word.substring(1, word.length() - 1);
+            char[] temp = subString.toCharArray();
+            Arrays.sort(temp);
+            String temps = new String(temp);
+            StringBuilder temBuilder = new StringBuilder(temps);
+            temBuilder.reverse();
+            temBuilder.insert(0, word.charAt(0));
+            temBuilder.append(word.charAt(word.length() - 1));
+            return temBuilder.toString();
+        }
+
+    }
+
+
 }
--- src/test/java/org/fundacionjala/coding/german/SortInnerConntentTest.java
+++ src/test/java/org/fundacionjala/coding/german/SortInnerConntentTest.java
@@ -1,7 +1,49 @@
 package org.fundacionjala.coding.german;
 
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by Administrator on 8/29/2017.
+ * Created by German on 8/29/2017.
  */
 public class SortInnerConntentTest {
+    /**
+     * Verify  sortInnerConntent method.
+     */
+    @Test
+    public void testSortInnerConntentMethod() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        String result = sortInnerContent.sortInnerConntent("sort the inner content in descending order");
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", result);
+    }
+
+    /**
+     * Verify  sortInnerConntent method.
+     */
+    @Test
+    public void testSortInnerConntentMethodOneWordChange() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        assertEquals("wiat for me", sortInnerContent.sortInnerConntent("wait for me"));
+    }
+
+    /**
+     * Verify  sortInnerConntent method.
+     */
+    @Test
+    public void testSortInnerConntentMethodAllWordsChange() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        assertEquals("tihs ktaa is esay", sortInnerContent.sortInnerConntent("this kata is easy"));
+    }
+
+    /**
+     * Verify  sortInnerConntent method.
+     */
+    @Test
+    public void testSortInnerConntentMethodWordsNotChange() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        assertEquals("I am", sortInnerContent.sortInnerConntent("I am"));
+    }
 }
+
