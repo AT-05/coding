7c5779a54d74a43cba912aec27df555581f34270
maizman <david.maizman@fundacion-org.com>
2017-08-30 22:30:17 +0000

    a

--- src/main/java/org/fundacionjala/coding/david/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/david/SortInnerContent.java
@@ -1,7 +1,43 @@
 package org.fundacionjala.coding.david;
 
+import java.util.Arrays;
+
 /**
- * Created by Administrator on 8/30/2017.
+ * This Class Word Reverse.
  */
 public class SortInnerContent {
+
+    /**
+     * This method word reverse only content of Word.
+     *
+     * @param words is value number.
+     * @return a value word reverse.
+     */
+    public String sortInnerReverse(String words) {
+
+        String[] listWord = words.split(" ");
+        StringBuilder listReverseWord = new StringBuilder();
+        String wordFirs, wordLast, wordContent;
+        int posStart, posEnd;
+        for (int c = 0; c < listWord.length; c++, listReverseWord.append(" ")) {
+
+            if (listWord[c].length() > 1) {
+                wordFirs = listWord[c].substring(0, 1);
+                posEnd = listWord[c].length();
+                posStart = posEnd - 1;
+                wordLast = listWord[c].substring(posStart, posEnd);
+                wordContent = listWord[c].substring(1, listWord[c].length() - 1);
+                listReverseWord.append(wordFirs);
+                String[] listOrder = wordContent.split("");
+                Arrays.sort(listOrder);
+                listReverseWord.append(new StringBuilder(String.join("", listOrder)).reverse().toString());
+                listReverseWord.append(wordLast);
+            } else {
+                listReverseWord.append(listWord[c]);
+            }
+        }
+
+        return listReverseWord.toString().trim();
+    }
 }
+
--- src/test/java/org/fundacionjala/coding/david/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/david/SortInnerContentTest.java
@@ -1,8 +1,34 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.david;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Administrator on 8/30/2017.
+ * This class realize test word reverse.
  */
 public class SortInnerContentTest {
+
+    /**
+     * This method word reserve.
+     */
+    @Test
+    public void reverseToStartInFivePosition() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+
+        final String actualResult = sortInnerContent.sortInnerReverse("sort the inner content in descending order");
+        final String actualResul2 = sortInnerContent.sortInnerReverse("wait for me");
+        final String actualResul3 = sortInnerContent.sortInnerReverse("I am");
+
+        final String expectResult = sortInnerContent.sortInnerReverse("srot the inner ctonnet in dsnnieedcg oredr");
+        final String expectResult2 = sortInnerContent.sortInnerReverse("wiat for me");
+        final String expectResult3 = sortInnerContent.sortInnerReverse("I am");
+
+        assertEquals(expectResult, actualResult);
+        assertEquals(expectResult2, sortInnerContent.sortInnerReverse(actualResul2));
+        assertEquals(expectResult3, sortInnerContent.sortInnerReverse(actualResul3));
+
+    }
+
 
 }
