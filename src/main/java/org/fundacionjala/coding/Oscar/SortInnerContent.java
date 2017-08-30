d4a7fa17bbe24351c2796d90dd68ef44a9432af3
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-08-30 02:53:37 -0400

    Adding Sort Inner Content - kata 5

--- src/main/java/org/fundacionjala/coding/Oscar/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/Oscar/SortInnerContent.java
@@ -1,7 +1,42 @@
 package org.fundacionjala.coding.Oscar;
 
+import java.util.Arrays;
+
 /**
  * Created by Oz64 on 30/08/2017.
  */
+
 public class SortInnerContent {
+    /**
+     * This method order the inner content of the words of the string descendent.
+     *
+     * @param content is the string with several words.
+     * @return change the content of the string words.
+     */
+    public String sortInnerContent(String content) {
+        String[] arrayString = content.split(" ");
+        for (int index = 0; index < arrayString.length; index++) {
+            arrayString[index] = sort(arrayString[index]);
+        }
+        return String.join(" ", arrayString);
+    }
+
+    /**
+     * This method change the content of one word.
+     *
+     * @param word is the word for change the content.
+     * @return the word with sort descendent.
+     */
+    public String sort(String word) {
+        char[] letters = word.toCharArray();
+        if (letters.length > 3) {
+            Arrays.sort(letters, 1, letters.length - 1);
+            for (int index = 1; index <= (letters.length / 2) - 1; index++) {
+                char aux = letters[index];
+                letters[index] = letters[letters.length - index - 1];
+                letters[letters.length - index - 1] = aux;
+            }
+        }
+        return String.valueOf(letters);
+    }
 }
--- src/test/java/org/fundacionjala/coding/Oscar/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/Oscar/SortInnerContentTest.java
@@ -1,7 +1,57 @@
 package org.fundacionjala.coding.Oscar;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Oz64 on 30/08/2017.
  */
 public class SortInnerContentTest {
+    private SortInnerContent kata5;
+
+    /**
+     * This method execute before any unit test.
+     */
+    @Before
+    public void setup() {
+        kata5 = new SortInnerContent();
+    }
+
+    /**
+     * Sort inner content with five words for changing.
+     */
+    @Test
+    public void testSortInnerContentOfFiveWords() {
+        String expectResult = kata5.sortInnerContent("sort the inner content in descending order");
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", expectResult);
+    }
+
+    /**
+     * Sort inner content with one word for changing.
+     */
+    @Test
+    public void testSortInnerContentOfOneWord() {
+        String expectResult = kata5.sortInnerContent("wait for me");
+        assertEquals("wiat for me", expectResult);
+    }
+
+    /**
+     * Sort inner content with three words for changing.
+     */
+    @Test
+    public void testSortInnerContentOfThreeWords() {
+        String expectResult = kata5.sortInnerContent("this kata is easy");
+        assertEquals("tihs ktaa is esay", expectResult);
+    }
+
+    /**
+     * Sort inner content with none words for changing.
+     */
+    @Test
+    public void testSortInnerContentOfNoneWords() {
+        String expectResult = kata5.sortInnerContent("I am");
+        assertEquals("I am", expectResult);
+    }
 }
