d99fdfbeedb2263321cc71e926f1439688ae6a19
GaryFN <gary.fuentes66@gmail.com>
2017-08-31 00:01:54 -0400

    kata5

--- src/main/java/org/fundacionjala/coding/Gary/InnerContent.java
+++ src/main/java/org/fundacionjala/coding/Gary/InnerContent.java
@@ -4,4 +4,33 @@ package org.fundacionjala.coding.Gary;
  * Created by Gary on 30/8/2017.
  */
 public class InnerContent {
+    private static final String DELIMITER = " ";
+    private String[] separateWords;
+
+    /**
+     * the method that converts.
+     *
+     * @param chain to convert.
+     * @return converted chain.
+     */
+    public String innerSort(String chain) {
+        separateWords = new String[]{};
+        separateWords = chain.split(DELIMITER);
+        for (int position = 0; position < separateWords.length; position++) {
+
+            if (separateWords[position].length() >= 3) {
+                String inner = separateWords[position].substring(1, separateWords[position].length() - 1);
+                inner = inner.chars().sorted()
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .reverse()
+                        .toString();
+                separateWords[position] = separateWords[position].substring(0, 1)
+                        .concat(inner)
+                        .concat(separateWords[position]
+                                .substring(separateWords[position].length() - 1, separateWords[position].length()));
+            }
+        }
+
+        return String.join(DELIMITER, separateWords);
+    }
 }
--- src/test/java/org/fundacionjala/coding/Gary/TestInnerContent.java
+++ src/test/java/org/fundacionjala/coding/Gary/TestInnerContent.java
@@ -1,7 +1,67 @@
 package org.fundacionjala.coding.Gary;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Gary on 30/8/2017.
  */
 public class TestInnerContent {
+    private InnerContent spin;
+
+    /**
+     * setup.
+     */
+    @Before
+    public void setup() {
+        spin = new InnerContent();
+    }
+
+
+    /**
+     * test first case.
+     */
+    @Test
+    public void testInnerContentCaseOne() {
+
+        String expectedResult = "srot the inner ctonnet in dsnnieedcg oredr";
+        String actualResult = spin.innerSort("sort the inner content in descending order");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * test second case.
+     */
+    @Test
+    public void testInnerContentSecondCase() {
+
+        String expectedResult = "wiat for me";
+        String actualResult = spin.innerSort("wait for me");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * test third case.
+     */
+    @Test
+    public void testInnerContentThirdCase() {
+
+        String expectedResult = "tihs ktaa is esay";
+        String actualResult = spin.innerSort("this kata is easy");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * test third case.
+     */
+    @Test
+    public void testInnerContentFourthCase() {
+
+        String expectedResult = "I am";
+        String actualResult = spin.innerSort("I am");
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
