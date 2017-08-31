ef3cde3302f3c6f1dec4a5f3d0d0debaf63e8178
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-08-31 15:45:41 -0400

    Abner_kata_4

--- src/main/java/org/fundacionjala/coding/abner/SpinWord.java
+++ src/main/java/org/fundacionjala/coding/abner/SpinWord.java
@@ -1,7 +1,34 @@
 package org.fundacionjala.coding.abner;
 
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Created by Administrator on 8/30/2017.
+ * This class spn the word
+ * Created by Abner on 8/30/2017.
  */
 public class SpinWord {
+
+    /**
+     * This function spin words.
+     * @param word string to spin.
+     * @return string turned.
+     */
+    public String spinWords(String word) {
+
+        String[] arr = word.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() > 4) {
+                String[] listString = arr[i].split("");
+                List<String> list = Arrays.asList(listString);
+                Collections.reverse(list);
+                String[] arraa = list.toArray(new String[]{});
+                arr[i] = String.join("", arraa);
+            }
+        }
+        return String.join(" ", arr);
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/abner/SpinWordsTest.java
+++ src/test/java/org/fundacionjala/coding/abner/SpinWordsTest.java
@@ -1,7 +1,26 @@
 package org.fundacionjala.coding.abner;
 
+import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
+
+
 /**
- * Created by Administrator on 8/30/2017.
+ * Created by Abner on 8/30/2017.
  */
 public class SpinWordsTest {
+    private SpinWord spinWord;
+
+    /**
+     * This test check the spin words.
+     */
+    @Test
+    public void testSpinWords() {
+        spinWord = new SpinWord();
+
+        assertEquals("Hey wollef sroirraw", spinWord.spinWords("Hey fellow warriors"));
+        assertEquals("This is a test", spinWord.spinWords("This is a test"));
+        assertEquals("This is rehtona test", spinWord.spinWords("This is another test"));
+    }
+
+
 }
