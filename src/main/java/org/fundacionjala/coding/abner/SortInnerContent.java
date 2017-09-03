94959cc68a272079fc7243553191f0b8dbb94b9c
AbnerMamani <30413596+AbnerMamani@users.noreply.github.com>
2017-09-03 11:06:28 -0400

    Abner kata 4 (#38)
    * Abner Kata 5
    
    * Add Kata 5

---
+++ src/main/java/org/fundacionjala/coding/abner/SortInnerContent.java
@@ -0,0 +1,34 @@
+package org.fundacionjala.coding.abner;
+
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 8/31/2017.
+ */
+public class SortInnerContent {
+
+    /**
+     * This function Sort Inner Content.
+     * @param cad this string not sort inner content.
+     * @return this string is Sort Inner Content.
+     */
+    public String sortInnert(String cad) {
+
+        String[] vector = cad.split(" ");
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i].length() > 1) {
+                StringBuilder result = new StringBuilder();
+                result.append(vector[i].substring(0, 1));
+
+                String[] betweenString = vector[i].substring(1, vector[i].length() - 1).split("");
+                Arrays.sort(betweenString);
+
+                result.append(new StringBuilder(String.join("", betweenString)).reverse().toString());
+                result.append(vector[i].substring(vector[i].length() - 1, vector[i].length()));
+                vector[i] = result.toString();
+            }
+        }
+        return String.join(" ", vector);
+
+    }
+}
