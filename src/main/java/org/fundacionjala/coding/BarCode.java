8e5cdb4d41215790a851c1a59d01ed6d04627465
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-09-04 12:13:36 -0400

    Add new fix for kata 6

--- src/main/java/org/fundacionjala/coding/BarCode.java
+++ src/main/java/org/fundacionjala/coding/BarCode.java
@@ -12,24 +12,15 @@ public class BarCode {
      * @return boolean.
      */
     public boolean checkCodeBar(String codBar) {
-        int checkSum = Integer.parseInt(codBar.substring(codBar.length() - 1));
         int sum = 0;
         for (int i = 1; i < codBar.length() - 1; i++) {
-            if (i % 2 == 0) {
-                sum += (int) (codBar.charAt(i));
-            } else {
-                sum += ((int) (codBar.charAt(i)) * 3);
-            }
+            int number =codBar.charAt(i);
+            sum += i % 2 == 0 ? number: number * 3;
         }
 
-        if (sum % 10 == 0) {
-            return (checkSum == 0) ? true : false;
+        int checksum = sum % 10 == 0 ? 0 : 10 - (sum % 10);
+        int lastNumber = Integer.parseInt(codBar.substring(codBar.length() - 1));
 
-        } else {
-            return ((10 - (sum % 10)) == checkSum) ? true : false;
-
-        }
-
-
+        return checksum == lastNumber;
     }
 }
