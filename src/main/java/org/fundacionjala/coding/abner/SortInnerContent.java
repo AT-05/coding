db39b2b1dc6f4821a72ebbf93cf3d9da03f8d496
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-09-04 15:34:04 -0400

    Fix the kata 5

--- src/main/java/org/fundacionjala/coding/abner/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/abner/SortInnerContent.java
@@ -9,21 +9,22 @@ public class SortInnerContent {
 
     /**
      * This function Sort Inner Content.
+     *
      * @param cad this string not sort inner content.
      * @return this string is Sort Inner Content.
      */
     public String sortInnert(String cad) {
-
-        String[] vector = cad.split(" ");
+        String delimiter = " ";
+        String[] vector = cad.split(delimiter);
         for (int i = 0; i < vector.length; i++) {
-            if (vector[i].length() > 1) {
+            if (vector[i].length() > 3) {
                 StringBuilder result = new StringBuilder();
                 result.append(vector[i].substring(0, 1));
 
                 String[] betweenString = vector[i].substring(1, vector[i].length() - 1).split("");
                 Arrays.sort(betweenString);
 
-                result.append(new StringBuilder(String.join("", betweenString)).reverse().toString());
+                result.append(new StringBuilder(String.join("", betweenString)).reverse());
                 result.append(vector[i].substring(vector[i].length() - 1, vector[i].length()));
                 vector[i] = result.toString();
             }
