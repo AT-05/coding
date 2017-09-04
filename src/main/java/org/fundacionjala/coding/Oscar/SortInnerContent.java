17ae2b2991a32f45c9356f32d0984cc87cf21ba6
oscarDelgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-04 16:19:53 -0400

    Adding fixes

--- src/main/java/org/fundacionjala/coding/Oscar/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/Oscar/SortInnerContent.java
@@ -2,6 +2,7 @@ package org.fundacionjala.coding.Oscar;
 
 import java.util.Arrays;
 
+
 /**
  * Created by Oz64 on 30/08/2017.
  */
@@ -16,7 +17,7 @@ public class SortInnerContent {
     public String sortInnerContent(String content) {
         String[] arrayString = content.split(" ");
         for (int index = 0; index < arrayString.length; index++) {
-            arrayString[index] = sort(arrayString[index]);
+            arrayString[index] = arrayString[index].length() > 3 ? sort(arrayString[index]) : arrayString[index];
         }
         return String.join(" ", arrayString);
     }
@@ -25,18 +26,13 @@ public class SortInnerContent {
      * This method change the content of one word.
      *
      * @param word is the word for change the content.
-     * @return the word with sort descendent.
+     * @return the word with sort descending.
      */
     public String sort(String word) {
         char[] letters = word.toCharArray();
-        if (letters.length > 3) {
-            Arrays.sort(letters, 1, letters.length - 1);
-            for (int index = 1; index <= (letters.length / 2) - 1; index++) {
-                char aux = letters[index];
-                letters[index] = letters[letters.length - index - 1];
-                letters[letters.length - index - 1] = aux;
-            }
-        }
-        return String.valueOf(letters);
+        Arrays.sort(letters, 1, letters.length - 1);
+        StringBuilder sb = new StringBuilder(String.valueOf(letters, 1, letters.length - 2));
+        sb.reverse();
+        return String.format("%c%s%c", letters[0], sb.toString(), letters[letters.length - 1]);
     }
 }
