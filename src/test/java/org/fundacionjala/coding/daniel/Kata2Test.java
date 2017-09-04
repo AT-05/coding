de5501c35908d2e2ffb423c454917e6808da3f5e
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2017-09-04 14:55:59 -0400

    add kata 2

--- src/test/java/org/fundacionjala/coding/daniel/Kata2Test.java
+++ src/test/java/org/fundacionjala/coding/daniel/Kata2Test.java
@@ -1,8 +1,21 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.daniel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 04/09/17.
  */
 public class Kata2Test {
+    /**
+     * Tests for Kata2.
+     */
+    @Test
+    public void test() {
+        assertEquals(23, Kata2.solution(10));
+        assertEquals(78, Kata2.solution(20));
+        assertEquals(9168, Kata2.solution(200));
+    }
 
 }
