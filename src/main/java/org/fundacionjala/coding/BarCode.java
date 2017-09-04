f035f4e6b45a3252bbf71b9a4379cb5b4287d62a
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-09-04 12:17:18 -0400

    Add new fix Kata 6

--- src/main/java/org/fundacionjala/coding/BarCode.java
+++ src/main/java/org/fundacionjala/coding/BarCode.java
@@ -8,14 +8,15 @@ public class BarCode {
 
     /**
      * This function check is the code bar is true.
+     *
      * @param codBar number de code bal.
      * @return boolean.
      */
     public boolean checkCodeBar(String codBar) {
         int sum = 0;
         for (int i = 1; i < codBar.length() - 1; i++) {
-            int number =codBar.charAt(i);
-            sum += i % 2 == 0 ? number: number * 3;
+            int number = codBar.charAt(i);
+            sum += i % 2 == 0 ? number : number * 3;
         }
 
         int checksum = sum % 10 == 0 ? 0 : 10 - (sum % 10);
