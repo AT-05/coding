4deb88c3d5497595bd79be5b3228ba15fca87e0d
maizman <david.maizman@fundacion-org.com>
2017-09-05 18:50:02 +0000

    Adding more unit test

--- src/test/java/org/fundacionjala/coding/david/EANValidationTest.java
+++ src/test/java/org/fundacionjala/coding/david/EANValidationTest.java
@@ -37,9 +37,21 @@ public class EANValidationTest {
     @Test
     public void checkSumWithValueZero() {
 
-        final boolean actualResult2 = eanValidation.checkSum("4003301018392");
-        assertFalse(actualResult2);
+        final boolean actualResult = eanValidation.checkSum("4003301018392");
+        assertFalse(actualResult);
 
     }
+
+    /**
+     * this method realize test of number 0. 9783827317100.
+     */
+    @Test
+    public void checkSumWithValueZero2() {
+
+        final boolean actualResult = eanValidation.checkSum("9783827317100");
+        assertTrue(actualResult);
+
+    }
+
 
 }
