81b37188fa11bae7c1451ce9f94bd81a217e71a4
FrancoDanielAldunateCordero <Franco.Aldunate@fundacion-jala.org>
2017-09-05 15:26:41 +0000

    Franco kata 4 (#34)
    * Adding Kata 4
    
    * Adding Kata 4
    
    * Adding Kata 4
    
    * Adding kata 4
    
    * Kata 4 fixed
    
    * Kata 4 fixed last
    
    * Kata 4 fix
    
    * Adding Kata 4 refactored

---
+++ src/test/java/org/fundacionjala/coding/Franco/SpinWordsTest.java
@@ -0,0 +1,45 @@
+package org.fundacionjala.coding.Franco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Administrator on 8/29/2017.
+ */
+public class SpinWordsTest {
+    private SpinWords spinWords;
+
+    /**
+     * This method tests instancing of HighAndLow Class.
+     */
+    @Before
+    public void setUp() {
+        spinWords = new SpinWords();
+    }
+
+    /**
+     * This method tests sentence with longer than 5 letters.
+     */
+    @Test
+    public void testSpinWordWithGreaterThan5LettersWords() {
+        final String expectedResult = "Hey wollef sroirraw";
+        final String actualResult = spinWords.spinWord("Hey fellow warriors");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests sentence with shorter than 5 letters.
+     */
+    @Test
+    public void testSpinWordWithSmallerThan5LettersWords() {
+        final String expectedResult = "This is a test";
+        final String actualResult = spinWords.spinWord("This is a test");
+        assertEquals(expectedResult, actualResult);
+
+        final String expectedResult2 = "This is rehtona test";
+        final String actualResult2 = spinWords.spinWord("This is another test");
+        assertEquals(expectedResult2, actualResult2);
+    }
+}
