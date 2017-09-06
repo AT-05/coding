ca4ae8525f4ad4ac06877c519ef08e1e9b3d9b67
atge <german.quinonez@fundacion-jala.com>
2017-09-06 10:57:58 -0400

    fix code review observation

--- src/test/java/org/fundacionjala/coding/german/SpinWordTest.java
+++ src/test/java/org/fundacionjala/coding/german/SpinWordTest.java
@@ -1,6 +1,7 @@
 package org.fundacionjala.coding.german;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,12 +10,21 @@ import static org.junit.Assert.assertEquals;
  * Created by German on 8/29/2017.
  */
 public class SpinWordTest {
+    private SpinWords spinWord;
+
+    /**
+     * This method execute before any test.
+     */
+    @Before
+    public void setUp() {
+        spinWord = new SpinWords();
+    }
+
     /**
      * Verify spinWords method.
      */
     @Test
     public void testSpinWordMethodTwoReverseWords() {
-        SpinWords spinWord = new SpinWords();
         assertEquals("Hey wollef sroirraw", spinWord.spinWords("Hey fellow warriors"));
     }
 
@@ -23,7 +33,6 @@ public class SpinWordTest {
      */
     @Test
     public void testSpinWordMethodZeroReveseWord() {
-        SpinWords spinWord = new SpinWords();
         assertEquals("This is a test", spinWord.spinWords("This is a test"));
     }
 
@@ -32,7 +41,6 @@ public class SpinWordTest {
      */
     @Test
     public void testSpinWordMethodOneReveseWord() {
-        SpinWords spinWord = new SpinWords();
         assertEquals("This is rehtona test", spinWord.spinWords("This is another test"));
     }
 
@@ -41,7 +49,6 @@ public class SpinWordTest {
      */
     @Test
     public void testSpinWordMethodOnlyOneNotReveseWord() {
-        SpinWords spinWord = new SpinWords();
         assertEquals("word", spinWord.spinWords("word"));
     }
 
@@ -50,7 +57,6 @@ public class SpinWordTest {
      */
     @Test
     public void testSpinWordMethodOnlyOneReveseWord() {
-        SpinWords spinWord = new SpinWords();
         assertEquals("sdrow", spinWord.spinWords("words"));
     }
 
