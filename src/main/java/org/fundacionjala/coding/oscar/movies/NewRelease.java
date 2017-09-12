3211f2c9682812ca55139194b63efff6bd5bd44a
oscarDelgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-12 13:35:11 -0400

    Adding kata 7(movies)

---
+++ src/main/java/org/fundacionjala/coding/oscar/movies/NewRelease.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.oscar.movies;
+
+/**
+ * Created by Oz64 on 09/09/2017.
+ */
+public class New_Release extends Movie {
+    public static final int NEW_RELEASE = 1;
+
+    /**
+     * This is the constructor, invoke the super class movie with
+     * the title ant the type of movie, 1 for this case of New_Release.
+     *
+     * @param title of the movie.
+     */
+    public New_Release(String title) {
+        super(title, NEW_RELEASE);
+    }
+
+    @Override
+    public double getAmount(int daysRented) {
+        return (daysRented * 3);
+    }
+}
---
+++ src/main/java/org/fundacionjala/coding/oscar/movies/Regular.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.oscar.movies;
+
+/**
+ * Created by Administrator on 9/8/2017.
+ */
+public class Regular {
+}
