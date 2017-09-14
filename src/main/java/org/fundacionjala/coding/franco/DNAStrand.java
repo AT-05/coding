ee9edf3eda7dba9090fcf4385cf5c3e98e20c39d
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-09-14 17:48:40 +0000

    Adding Kata 8

--- src/main/java/org/fundacionjala/coding/franco/DNAStrand.java
+++ src/main/java/org/fundacionjala/coding/franco/DNAStrand.java
@@ -4,4 +4,45 @@ package org.fundacionjala.coding.franco;
  * Created by Administrator on 9/14/2017.
  */
 public class DNAStrand {
+    private static final String[][] DNA_COMPLEMENTS = {{"A", "T"}, {"T", "A"}, {"C", "G"}, {"G", "C"}};
+    public static final int CONS_ZERO = 0;
+    public static final int CONS_ONE = 1;
+
+    /**
+     * This method appends complement of a DNAStrand.
+     *
+     * @param chain is a DNAStrand.
+     * @return the complement of this DNAStrand.
+     */
+    public String makeComplement(String chain) {
+        final StringBuilder result = new StringBuilder();
+        final String[] dna = chain.split("");
+
+        int i = CONS_ZERO;
+        while (i < dna.length) {
+            result.append(getComplement(dna[i]));
+            i++;
+        }
+        return result.toString();
+    }
+
+    /**
+     * This method finds the complement of an individual Letter.
+     *
+     * @param character is a Letter.
+     * @return the complement of this letter.
+     */
+    public String getComplement(String character) {
+        String result = "";
+        int i = 0;
+
+        while (i < DNA_COMPLEMENTS.length) {
+            if (DNA_COMPLEMENTS[i][CONS_ZERO].equals(character)) {
+                result = DNA_COMPLEMENTS[i][CONS_ONE];
+                break;
+            }
+            i++;
+        }
+        return result;
+    }
 }
--- src/test/java/org/fundacionjala/coding/franco/DNAStrandTest.java
+++ src/test/java/org/fundacionjala/coding/franco/DNAStrandTest.java
@@ -1,8 +1,100 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.franco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 9/14/2017.
  */
 public class DNAStrandTest {
+    private DNAStrand dnaStrand;
 
+    /**
+     * This method test instancing of Class DNAStrand.
+     */
+    @Before
+    public void setUp() {
+        dnaStrand = new DNAStrand();
+    }
+
+    /**
+     * This method tests output complement of AAAA DNAStrand.
+     */
+    @Test
+    public void makeComplementOfAAAA() {
+        final String actualResult = dnaStrand.makeComplement("AAAA");
+        final String expectedResult = "TTTT";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of ATTGC DNAStrand.
+     */
+    @Test
+    public void makeComplementOfATTGC() {
+        final String actualResult = dnaStrand.makeComplement("ATTGC");
+        final String expectedResult = "TAACG";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of GTAT DNAStrand.
+     */
+    @Test
+    public void makeComplementOfGTAT() {
+        final String actualResult = dnaStrand.makeComplement("GTAT");
+        final String expectedResult = "CATA";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of AAGG DNAStrand.
+     */
+    @Test
+    public void makeComplementOfAAGG() {
+        final String actualResult = dnaStrand.makeComplement("AAGG");
+        final String expectedResult = "TTCC";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of CGCG DNAStrand.
+     */
+    @Test
+    public void makeComplementOfCGCG() {
+        final String actualResult = dnaStrand.makeComplement("CGCG");
+        final String expectedResult = "GCGC";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of a long DNAStrand chain.
+     */
+    @Test
+    public void makeComplementOfLongChain() {
+        final StringBuilder chain = new StringBuilder("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAA");
+        chain.append("TATATATATATACGAGAGAATACAGATAGACAGATTA");
+        final String actualResult = dnaStrand.makeComplement(chain.toString());
+        final String expectedResult = "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT";
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * This method tests output complement of CGRG DNAStrand.
+     */
+    @Test
+    public void makeComplementOfCGRG() {
+        final String actualResult = dnaStrand.makeComplement("CGRG");
+        final String expectedResult = "GCC";
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
