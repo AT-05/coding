d5dc8d7c1336ec4f0bb9a7c6a949bda73eb0d9af
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-09-14 19:56:14 +0000

    Adding Kata 10

--- src/main/java/org/fundacionjala/coding/franco/PersistentBugger.java
+++ src/main/java/org/fundacionjala/coding/franco/PersistentBugger.java
@@ -1,7 +1,32 @@
 package org.fundacionjala.coding.franco;
 
+import java.util.stream.Stream;
+
 /**
  * Created by Administrator on 9/14/2017.
  */
 public class PersistentBugger {
+    private int counter = 0;
+
+    /**
+     * This method calculates persistence aof a number.
+     *
+     * @param number is an integer number.
+     * @return number of multiplications between the digits of the number.
+     */
+    public int persistence(int number) {
+        final int[] numbers = Stream.of(String.valueOf(number).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int result = 1;
+        while (number > 9 || numbers.length > 1) {
+            counter++;
+            for (int i = 0; i < numbers.length; i++) {
+                result *= numbers[i];
+            }
+            return persistence(result);
+        }
+        return counter;
+    }
 }
--- src/test/java/org/fundacionjala/coding/franco/PersistentBuggerTest.java
+++ src/test/java/org/fundacionjala/coding/franco/PersistentBuggerTest.java
@@ -1,8 +1,61 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.franco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 9/14/2017.
  */
 public class PersistentBuggerTest {
+    private PersistentBugger persistentBugger;
 
+    /**
+     * This method tests instancing of Class PersistentBugger.
+     */
+    @Before
+    public void setUp() {
+        persistentBugger = new PersistentBugger();
+    }
+
+    /**
+     * This method tests result of  persistence of number 39.
+     */
+    @Test
+    public void persistenceOf39() {
+        assertEquals(3, persistentBugger.persistence(39));
+    }
+
+    /**
+     * This method tests result of  persistence of number 4.
+     */
+    @Test
+    public void persistenceOf4() {
+        assertEquals(0, persistentBugger.persistence(4));
+    }
+
+    /**
+     * This method tests result of  persistence of number 25.
+     */
+    @Test
+    public void persistenceOf25() {
+        assertEquals(2, persistentBugger.persistence(25));
+    }
+
+    /**
+     * This method tests result of  persistence of number 999.
+     */
+    @Test
+    public void persistenceOf999() {
+        assertEquals(4, persistentBugger.persistence(999));
+    }
+
+    /**
+     * This method tests result of persistence of number 444.
+     */
+    @Test
+    public void persistenceOf444() {
+        assertEquals(3, persistentBugger.persistence(444));
+    }
 }
