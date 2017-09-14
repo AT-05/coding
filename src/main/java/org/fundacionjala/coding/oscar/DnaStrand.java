1afc6aabaecfcce3d264aa3a93bed32f309ad61c
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-14 19:19:37 -0400

    Adding code for Kata 8

--- src/main/java/org/fundacionjala/coding/oscar/DnaStrand.java
+++ src/main/java/org/fundacionjala/coding/oscar/DnaStrand.java
@@ -3,10 +3,39 @@ package org.fundacionjala.coding.oscar;
 /**
  * Created by Oz64 on 14/09/2017.
  */
-public class ComplementaryDNA {
+public final class DnaStrand {
+    /**
+     * constructor.
+     */
+    private DnaStrand() {
+    }
+
+    /**
+     * This method change the letter for them complement.
+     *
+     * @param stringDNA is the DNA string.
+     * @return the new string.
+     */
     public static String makeComplement(String stringDNA) {
-        stringDNA = stringDNA.replace("A","T");
-        stringDNA = stringDNA.replace("C","G");
-        return stringDNA;
+        String[] list = stringDNA.split("");
+        for (int index = 0; index < list.length; index++) {
+            switch (list[index]) {
+                case "A":
+                    list[index] = "T";
+                    break;
+                case "T":
+                    list[index] = "A";
+                    break;
+                case "C":
+                    list[index] = "G";
+                    break;
+                case "G":
+                    list[index] = "C";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return String.join("", list);
     }
 }
--- src/test/java/org/fundacionjala/coding/oscar/DnaStrandTest.java
+++ src/test/java/org/fundacionjala/coding/oscar/DnaStrandTest.java
@@ -7,17 +7,63 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Oz64 on 14/09/2017.
  */
-public class ComplementaryDNATest {
-    @Test
-    public void testComplement() {
-        assertEquals("TAACG", ComplementaryDNA.makeComplement("ATTGC"));
+public class DnaStrandTest {
+    /**
+     * Test 01.
+     */
+   @Test
+    public void test01() {
+        assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
     }
 
+    /**
+     * Test 02.
+     */
     @Test
-    public void testComplement2() {
-        String expectedResult = "CATA";
-        String actualResult = ComplementaryDNA.makeComplement("GTAT");
-        expectedResult.equals(actualResult);
+    public void test02() {
+        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+
+    /**
+     * Test 03.
+     */
+    @Test
+    public void test03() {
+        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+    }
+
+    /**
+     * Test 04.
+     */
+    @Test
+    public void test04() {
+        assertEquals("TTCC", DnaStrand.makeComplement("AAGG"));
+    }
+
+    /**
+     * Test 05.
+     */
+    @Test
+    public void test05() {
+        assertEquals("GCGC", DnaStrand.makeComplement("CGCG"));
+    }
+
+    /**
+     * Test 06.
+     */
+    @Test
+    public void test06() {
+        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+
+    /**
+     * Test 07.
+     */
+    @Test
+    public void test07() {
+        String expectedResult = "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT";
+        String actualResult = "GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA";
+        assertEquals(expectedResult, DnaStrand.makeComplement(actualResult));
     }
 
 }
