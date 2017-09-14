5793efd5ab3653fac95f1fc978aa12fa64d6e43b
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-09-14 18:46:39 +0000

    Adding Kata 9

--- src/main/java/org/fundacionjala/coding/franco/PlanetTwisted.java
+++ src/main/java/org/fundacionjala/coding/franco/PlanetTwisted.java
@@ -1,7 +1,25 @@
 package org.fundacionjala.coding.franco;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * Created by Administrator on 9/14/2017.
  */
 public class PlanetTwisted {
+    final static int CONS_THREE = 3;
+    final static int CONS_SEVEN = 7;
+
+    public Integer[] sortNumbers(Integer[] chainOfNumbers) {
+        final int[] numbers = Arrays.stream(chainOfNumbers).mapToInt(Integer::intValue).toArray();
+        final int[] sortedNumbers = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            sortedNumbers[i] = numbers[i] == CONS_THREE ? CONS_SEVEN : numbers[i] == CONS_SEVEN ? CONS_THREE : numbers[i];
+        }
+        //Arrays.sort(sortedNumbers);
+        Integer[] result = IntStream.of(sortedNumbers).boxed().toArray( Integer[]::new );
+
+        return result;
+    }
 }
--- src/test/java/org/fundacionjala/coding/franco/PlanetTwistedTest.java
+++ src/test/java/org/fundacionjala/coding/franco/PlanetTwistedTest.java
@@ -1,8 +1,33 @@
+package org.fundacionjala.coding.franco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Administrator on 9/14/2017.
  */
 public class PlanetTwistedTest {
+    private PlanetTwisted planetTwisted;
 
+    /**
+     * This method tests instancing of Class PlanetTwisted.
+     */
+    @Before
+    public void setUp() {
+        planetTwisted = new PlanetTwisted();
+    }
+
+    @Test
+    public void sortNumbersBasicCase() {
+        assertEquals(Arrays.toString(new Integer[] {1,2,7,4,5,6,3,8,9}), Arrays.toString(planetTwisted.sortNumbers(new Integer[] {1,2,3,4,5,6,7,8,9})));
+    }
+
+    @Test
+    public void sortNumbers() {
+        assertEquals(Arrays.toString(new Integer[] {12,14,13}), Arrays.toString(planetTwisted.sortNumbers(new Integer[] {12,13,14})));
+    }
 }
