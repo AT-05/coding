0afece4772fe2304de4a7fd8e79685764c6b5360
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-15 00:57:30 -0400

    Fix setup test

--- src/test/java/org/fundacionjala/coding/oscar/SortTwisted37Test.java
+++ src/test/java/org/fundacionjala/coding/oscar/SortTwisted37Test.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.coding.oscar;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,12 +11,21 @@ import static org.junit.Assert.assertEquals;
  * Created by Oz64 on 14/09/2017.
  */
 public class SortTwisted37Test {
+    private SortTwisted37 sortTwisted37;
+
+    /**
+     * This executed before all test.
+     */
+    @Before
+    public void setUp() {
+        sortTwisted37 = new SortTwisted37();
+    }
+
     /**
      * Test basic.
      */
     @Test
     public void basicTests() {
-        SortTwisted37 sortTwisted37 = new SortTwisted37();
         assertEquals(Arrays.toString(new Integer[]{1, 2, 7, 4, 5, 6, 3, 8, 9}),
                 Arrays.toString(sortTwisted37
                         .sortTwisted(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9})));
@@ -32,7 +42,6 @@ public class SortTwisted37Test {
      */
     @Test
     public void unChangedArrayTest() {
-        SortTwisted37 sortTwisted37 = new SortTwisted37();
         Integer[] array = new Integer[]{9, 2, 4, 7, 3};
         Integer[] arrayCpy = new Integer[]{9, 2, 4, 7, 3};
         assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}), Arrays.toString(sortTwisted37.sortTwisted(array)));
