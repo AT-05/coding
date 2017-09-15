af005fd10206cc39d39870e6af51a87605b24f77
marcomendez <marco.mendez@fundacion-jala.org>
2017-09-15 23:39:57 +0000

    Adding my exam

--- src/main/java/org/fundacionjala/coding/marco/Examen.java
+++ src/main/java/org/fundacionjala/coding/marco/Examen.java
@@ -1,4 +1,16 @@
 package org.fundacionjala.coding.marco;
 
+/**
+ * This was made by Marco Mendez September 15, 2017.
+ */
 public class Examen {
+    /**
+     * This method calculate digital root.
+     *
+     * @param value test.
+     * @return test.
+     */
+    public int root(int value) {
+        return (value != 0 && value % 9 == 0) ? 9 : value % 9;
+    }
 }
--- src/test/java/org/fundacionjala/coding/marco/ExamenTest.java
+++ src/test/java/org/fundacionjala/coding/marco/ExamenTest.java
@@ -1,4 +1,60 @@
 package org.fundacionjala.coding.marco;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * This was made by Marco Mendez September 15, 2017.
+ */
 public class ExamenTest {
+
+    private Examen digital;
+    /**
+     * This start the object.
+     */
+    @Before
+    public void ini() {
+        digital = new Examen();
+    }
+
+    /**
+     * Test 1.
+     */
+    @Test
+    public void test1() {
+        final int expectedResult = 7;
+        final int actualResult = 16;
+        assertEquals(expectedResult, digital.root(actualResult));
+    }
+
+    /**
+     * Test 2.
+     */
+    @Test
+    public void test2() {
+        final int expectedResult = 6;
+        final int actualResult = 942;
+        assertEquals(expectedResult, digital.root(actualResult));
+    }
+
+    /**
+     * Test 3.
+     */
+    @Test
+    public void test3() {
+        final int expectedResult = 6;
+        final int actualResult = 132189;
+        assertEquals(expectedResult, digital.root(actualResult));
+    }
+
+    /**
+     * Test 4.
+     */
+    @Test
+    public void test4() {
+        final int expectedResult = 2;
+        final int actualResult = 493193;
+        assertEquals(expectedResult, digital.root(actualResult));
+    }
 }
