92ac401f79217d406f18006d44af6cfd4a18e2f9
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-15 02:13:53 -0400

    Adding Kata 10 code

--- src/main/java/org/fundacionjala/coding/oscar/Persist.java
+++ src/main/java/org/fundacionjala/coding/oscar/Persist.java
@@ -4,4 +4,24 @@ package org.fundacionjala.coding.oscar;
  * Created by Oz64 on 15/09/2017.
  */
 public class Persist {
+    /**
+     * This method return the number of times that multiply.
+     *
+     * @param number as integer.
+     * @return the times that multiply as integer.
+     */
+    public int persistence(int number) {
+        int total = 0;
+        if (number < 10) {
+            return 0;
+        } else {
+            String[] numbers = String.valueOf(number).split("");
+            int resp = Integer.parseInt(numbers[0]);
+            for (int index = 1; index < numbers.length; index++) {
+                resp *= Integer.parseInt(numbers[index]);
+                total = resp;
+            }
+        }
+        return persistence(total) + 1;
+    }
 }
--- src/test/java/org/fundacionjala/coding/oscar/PersistTest.java
+++ src/test/java/org/fundacionjala/coding/oscar/PersistTest.java
@@ -1,7 +1,33 @@
 package org.fundacionjala.coding.oscar;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Oz64 on 15/09/2017.
  */
 public class PersistTest {
+    private Persist persist;
+
+    /**
+     * This executed before any test.
+     */
+    @Before
+    public void setUp() {
+        persist = new Persist();
+    }
+
+    /**
+     * Basic test.
+     */
+    @Test
+    public void basicTests() {
+        assertEquals(3, persist.persistence(39));
+        assertEquals(0, persist.persistence(4));
+        assertEquals(2, persist.persistence(25));
+        assertEquals(4, persist.persistence(999));
+        assertEquals(3, persist.persistence(444));
+    }
 }
