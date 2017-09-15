159e3253ffac72b4be48e3245719946529f8b9b6
atge <german.quinonez@fundacion-jala.com>
2017-09-15 18:30:12 -0400

    adding kata 11

--- src/main/java/org/fundacionjala/coding/german/DigitalRoot.java
+++ src/main/java/org/fundacionjala/coding/german/DigitalRoot.java
@@ -4,4 +4,37 @@ package org.fundacionjala.coding.german;
  * Created by Administrator on 9/15/2017.
  */
 public class DigitalRoot {
+    /**
+     * This method calculates root is the recursive sum of all the digits in a number.
+     *
+     * @param number int
+     * @return int sum digits
+     */
+
+    public int digitalRoot(int number) {
+        int res = number;
+        if (res > 10) {
+
+            res = digitalRoot(sumDig(res));
+        }
+        return res;
+    }
+
+    /**
+     * This method return sum of all the digits in a number.
+     *
+     * @param dig number
+     * @return int sum digits
+     */
+    private int sumDig(int dig) {
+        int res = 0;
+        Integer interge = dig;
+        String s = interge.toString();
+        for (int i = 0; i < s.length(); i++) {
+            res += Character.getNumericValue(s.charAt(i));
+        }
+        return res;
+
+
+    }
 }
--- src/test/java/org/fundacionjala/coding/german/DigitalRootTest.java
+++ src/test/java/org/fundacionjala/coding/german/DigitalRootTest.java
@@ -1,7 +1,65 @@
 package org.fundacionjala.coding.german;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 9/15/2017.
  */
 public class DigitalRootTest {
+    private DigitalRoot digitalRoot;
+
+    /**
+     * This method execute before any test.
+     */
+    @Before
+    public void setUp() {
+        digitalRoot = new DigitalRoot();
+    }
+
+    /**
+     * Verify digitalRoot method.
+     */
+    @Test
+    public void testDigitalRootWithTwoDigits() {
+        final int expectedResult = 7;
+        final int actualResul = digitalRoot.digitalRoot(16);
+        assertEquals(expectedResult, actualResul);
+
+    }
+
+    /**
+     * Verify digitalRoot method.
+     */
+    @Test
+    public void testDigitalRootWithThreeDigits() {
+        final int expectedResult = 6;
+        final int actualResul = digitalRoot.digitalRoot(942);
+        assertEquals(expectedResult, actualResul);
+
+    }
+
+    /**
+     * Verify digitalRoot method.
+     */
+    @Test
+    public void testDigitalRootWithSixDigits() {
+        final int expectedResult = 6;
+        final int actualResul = digitalRoot.digitalRoot(132189);
+        assertEquals(expectedResult, actualResul);
+
+    }
+
+    /**
+     * Verify digitalRoot method.
+     */
+    @Test
+    public void testDigitalRootWithOthersSixDigits() {
+        final int expectedResult = 2;
+        final int actualResul = digitalRoot.digitalRoot(493193);
+        assertEquals(expectedResult, actualResul);
+
+    }
 }
