500f0546df3542a9236c14bf630c90b2b9b318d2
marcomendez <marco.mendez@fundacion-jala.org>
2017-09-15 23:23:17 +0000

    Adding Kata 8

--- src/main/java/org/fundacionjala/coding/marco/DnaStrand.java
+++ src/main/java/org/fundacionjala/coding/marco/DnaStrand.java
@@ -1,4 +1,21 @@
 package org.fundacionjala.coding.marco;
 
+/**
+ * This was made by Marco Mendez September 15, 2015.
+ */
 public class DnaStrand {
+    /**
+     * This method test.
+     * @param string test.
+     * @return test.
+     */
+    public  String makeComplement(String string) {
+        return string.replace('A', '1')
+                .replace('C', '2')
+                .replace('T', 'A')
+                .replace('G', 'C')
+                .replace('1', 'T')
+                .replace('2', 'G');
+
+    }
 }
--- src/test/java/org/fundacionjala/coding/marco/DnaStrandTest.java
+++ src/test/java/org/fundacionjala/coding/marco/DnaStrandTest.java
@@ -1,4 +1,91 @@
 package org.fundacionjala.coding.marco;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This was made by Marco Mendez September 15, 2015.
+ */
 public class DnaStrandTest {
+    private DnaStrand dna;
+
+    /**
+     * Method ini.
+     */
+    @Before
+    public void ini() {
+        dna = new DnaStrand();
+    }
+
+    /**
+     * Scenario 1.
+     */
+    @Test
+    public void test01() {
+        final String expectResult = "TTTT";
+        final String actualResult = "AAAA";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario 2.
+     */
+    @Test
+    public void test02() {
+        final String expectResult = "TAACG";
+        final String actualResult = "ATTGC";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario 3.
+     */
+    @Test
+    public void test03() {
+        final String expectResult = "CATA";
+        final String actualResult = "GTAT";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario 4.
+     */
+    @Test
+    public void test04() {
+        final String expectResult = "TTCC";
+        final String actualResult = "AAGG";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario5.
+     */
+    @Test
+    public void test05() {
+        final String expect = "GCGC";
+        final String actualResult = "CGCG";
+        assertEquals(expect, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario6.
+     */
+    @Test
+    public void test06() {
+        final String expectResult = "TAACG";
+        final String actualResult = "ATTGC";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
+
+    /**
+     * Scenario 7.
+     */
+    @Test
+    public void test07() {
+        final String expectResult = "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT";
+        final String actualResult = "GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA";
+        assertEquals(expectResult, dna.makeComplement(actualResult));
+    }
 }
