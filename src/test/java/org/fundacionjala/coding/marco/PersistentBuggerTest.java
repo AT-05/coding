01c21703c37a7abcd12fddc4eb4ee362b84c9d91
marcomendez <marco.mendez@fundacion-jala.org>
2017-09-15 23:34:11 +0000

    Adding Kata 10

--- src/test/java/org/fundacionjala/coding/marco/PersistentBuggerTest.java
+++ src/test/java/org/fundacionjala/coding/marco/PersistentBuggerTest.java
@@ -1,16 +1,74 @@
 package org.fundacionjala.coding.marco;
 
-public class PersistentBugger {
-    public int persistence(int value) {
-        while (value >= 10) {
+import org.junit.Before;
+import org.junit.Test;
 
-            String[] vectorNumber = String.valueOf(value).split("");
-            value = 0;
-            for (String aVectorNumber : vectorNumber) {
-                value += Integer.parseInt(aVectorNumber);
-            }
+import static org.junit.Assert.assertEquals;
 
-        }
-        return value;
+/**
+ * This was made by Marco Mendez September 15, 2017.
+ */
+public class PersistentBuggerTest {
+
+    private PersistentBugger persistence;
+
+    /**
+     * this method start the object.
+     */
+    @Before
+    public void ini() {
+        persistence = new PersistentBugger();
+    }
+
+    /**
+     * Test 1.
+     */
+    @Test
+    public void test1() {
+        final int expectResult = 3;
+        final int actualResult = 39;
+        assertEquals(expectResult, persistence.persistence(actualResult));
+    }
+
+    /**
+     * Test 2.
+     */
+    @Test
+    public void test2() {
+        final int expectResult = 2;
+        final int actualResult = 25;
+        assertEquals(expectResult, persistence.persistence(actualResult));
+
+    }
+
+    /**
+     * Test 3.
+     */
+    @Test
+    public void test3() {
+        final int expectResult = 4;
+        final int actualResult = 999;
+        assertEquals(expectResult, persistence.persistence(actualResult));
+
+    }
+
+    /**
+     * Test 4.
+     */
+    @Test
+    public void test4() {
+        final int expectResult = 3;
+        final int actualResult = 444;
+        assertEquals(expectResult, persistence.persistence(actualResult));
+    }
+
+    /**
+     * Test 5.
+     */
+    @Test
+    public void test5() {
+        final int expectResult = 0;
+        final int actualResult = 4;
+        assertEquals(expectResult, persistence.persistence(actualResult));
     }
 }
