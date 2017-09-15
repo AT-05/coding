c53efa2fbfb8664fd05fb9af743a1c63be382317
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-15 00:42:15 -0400

    Adding sort twisted 3-7 code

--- src/main/java/org/fundacionjala/coding/oscar/SortTwisted37.java
+++ src/main/java/org/fundacionjala/coding/oscar/SortTwisted37.java
@@ -1,7 +1,46 @@
 package org.fundacionjala.coding.oscar;
 
+import java.util.Arrays;
+
 /**
  * Created by Oz64 on 14/09/2017.
  */
 public class SortTwisted37 {
+
+    /**
+     * This method sort the array of numbers and call the method for change the numbers.
+     * @param array of the numbers.
+     * @return array numbers sort and twisted.
+     */
+    public Integer[] sortTwisted(Integer[] array) {
+        Integer[] arrayNumbers = array.clone();
+        Arrays.sort(arrayNumbers);
+        for (int index = 0; index < arrayNumbers.length; index++) {
+            arrayNumbers[index] = change3and7(arrayNumbers[index]);
+        }
+        return arrayNumbers;
+    }
+
+    /**
+     * This method change if are a 3 for 7 and 7 for 3.
+     * @param number as a integer.
+     * @return the number as a integer.
+     */
+    public int change3and7(int number) {
+        String num = String.valueOf(number);
+        String[] list = num.split("");
+        for (int index = 0; index < list.length; index++) {
+            switch (list[index]) {
+                case "3":
+                    list[index] = "7";
+                    break;
+                case "7":
+                    list[index] = "3";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return Integer.parseInt(String.join("", list));
+    }
 }
--- src/test/java/org/fundacionjala/coding/oscar/SortTwisted37Test.java
+++ src/test/java/org/fundacionjala/coding/oscar/SortTwisted37Test.java
@@ -1,7 +1,41 @@
 package org.fundacionjala.coding.oscar;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Oz64 on 14/09/2017.
  */
 public class SortTwisted37Test {
+    /**
+     * Test basic.
+     */
+    @Test
+    public void basicTests() {
+        SortTwisted37 sortTwisted37 = new SortTwisted37();
+        assertEquals(Arrays.toString(new Integer[]{1, 2, 7, 4, 5, 6, 3, 8, 9}),
+                Arrays.toString(sortTwisted37
+                        .sortTwisted(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9})));
+        /*assertEquals(Arrays.toString(new Integer[]{12, 14, 13}),
+                Arrays.toString(sortTwisted37
+                        .sortTwisted((new Integer[]{12, 13, 14}))));*/
+        assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}),
+                Arrays.toString(sortTwisted37
+                        .sortTwisted(new Integer[]{9, 2, 4, 7, 3})));
+    }
+
+    /**
+     * Test array.
+     */
+    @Test
+    public void unChangedArrayTest() {
+        SortTwisted37 sortTwisted37 = new SortTwisted37();
+        Integer[] array = new Integer[]{9, 2, 4, 7, 3};
+        Integer[] arrayCpy = new Integer[]{9, 2, 4, 7, 3};
+        assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}), Arrays.toString(sortTwisted37.sortTwisted(array)));
+        assertEquals("You should not modify the input array!", Arrays.toString(arrayCpy), Arrays.toString(array));
+    }
 }
