f9b8ecbf5818477b9503e07d2b925e2de009d69b
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-09-15 18:20:44 +0000

    Adding kata final complete

--- src/main/java/org/fundacionjala/coding/franco/DigitalRoot.java
+++ src/main/java/org/fundacionjala/coding/franco/DigitalRoot.java
@@ -1,7 +1,36 @@
 package org.fundacionjala.coding.franco;
 
+import java.util.stream.Stream;
+
 /**
  * Created by Administrator on 9/15/2017.
  */
 public class DigitalRoot {
+
+    public static final int CONS_ONE = 1;
+    private int lastSum = 0;
+
+    /**
+     * This method sums digits of a number until the
+     * result is one digit.
+     *
+     * @param number to work with.
+     * @return the last result of the sum of the
+     * digits of the number.
+     */
+    public int sum(int number) {
+        final int[] numbers = Stream.of(String.valueOf(number).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        if (numbers.length <= CONS_ONE) {
+            return lastSum;
+        }
+        int sum = 0;
+        for (int number1 : numbers) {
+            sum += number1;
+        }
+        lastSum = sum;
+        return sum(lastSum);
+    }
 }
