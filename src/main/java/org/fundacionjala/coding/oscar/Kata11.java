a8f55726185b4ff02a36f7d8dbc22ba1d242714e
oscarDelgadillo <oscar.delgadillo@fundacion-jala.org>
2017-09-15 18:19:18 -0400

    Adding code for kata 11

--- src/main/java/org/fundacionjala/coding/oscar/Kata11.java
+++ src/main/java/org/fundacionjala/coding/oscar/Kata11.java
@@ -4,4 +4,23 @@ package org.fundacionjala.coding.oscar;
  * Created by Administrator on 9/15/2017.
  */
 public class Kata11 {
+    /**
+     * This method sum all numbers of the integer.
+     *
+     * @param number as a integer.
+     * @return total sum as a integer.
+     */
+    public int digitalRoot(int number) {
+        int total = 0;
+        String[] numbers = String.valueOf(number).split("");
+        int resp = Integer.parseInt(numbers[0]);
+        for (int index = 1; index < numbers.length; index++) {
+            resp += Integer.parseInt(numbers[index]);
+            total = resp;
+        }
+        if (total > 10) {
+            return digitalRoot(total);
+        }
+        return total;
+    }
 }
--- src/test/java/org/fundacionjala/coding/oscar/Kata11Test.java
+++ src/test/java/org/fundacionjala/coding/oscar/Kata11Test.java
@@ -1,7 +1,32 @@
 package org.fundacionjala.coding.oscar;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 9/15/2017.
  */
 public class Kata11Test {
+    private Kata11 kata11;
+
+    /**
+     * This method execute before all test.
+     */
+    @Before
+    public void setUp() {
+        kata11 = new Kata11();
+    }
+
+    /**
+     * This test verify the digitalRoot method.
+     */
+    @Test
+    public void test() {
+        assertEquals(7, kata11.digitalRoot(16));
+        assertEquals(6, kata11.digitalRoot(942));
+        assertEquals(6, kata11.digitalRoot(132189));
+        assertEquals(2, kata11.digitalRoot(493193));
+    }
 }
