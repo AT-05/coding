7aa3a81d10686981cfde3b92534f5f3fed9e7c5e
marcomendez <marco.mendez@fundacion-jala.org>
2017-09-15 22:15:16 +0000

    add

--- src/test/java/org/fundacionjala/coding/Marco/Kata_ExamTest.java
+++ src/test/java/org/fundacionjala/coding/Marco/Kata_ExamTest.java
@@ -1,4 +1,39 @@
 package org.fundacionjala.coding.Marco;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Kata_ExamTest {
+    Kata_Exam digital;
+    @Before
+    public void ini()
+    {
+     digital=new Kata_Exam();
+    }
+    @Test
+    public void test1()
+    {
+
+        assertEquals(7,digital.Root(16));
+    }
+    @Test
+    public void test2()
+    {
+        Kata_Exam digital=new Kata_Exam();
+        assertEquals(6,digital.Root(942));
+    }
+    @Test
+    public void test3()
+    {
+        Kata_Exam digital=new Kata_Exam();
+        assertEquals(6,digital.Root(132189));
+    }
+    @Test
+    public void test4()
+    {
+        Kata_Exam digital=new Kata_Exam();
+        assertEquals(2,digital.Root(493193));
+    }
 }
