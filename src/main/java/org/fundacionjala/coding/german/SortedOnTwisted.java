76b58a9295876c83d3f8eebabc97f8c6a4f6ef7e
atge <german.quinonez@fundacion-jala.com>
2017-09-15 20:39:57 -0400

    adding kata 9

--- src/main/java/org/fundacionjala/coding/german/SortedOnTwisted.java
+++ src/main/java/org/fundacionjala/coding/german/SortedOnTwisted.java
@@ -1,7 +1,37 @@
 package org.fundacionjala.coding.german;
 
+import java.util.Arrays;
+
 /**
  * Created by Administrator on 9/15/2017.
  */
 public class SortedOnTwisted {
+    /**
+     * This method swap three seven.
+     *
+     * @param integer to swap
+     * @return Integer
+     */
+    private Integer swapThreeSeven(Integer integer) {
+        return Integer.valueOf(integer.toString()
+                .replace("7", "_")
+                .replace("3", "7")
+                .replace("_", "3")
+        );
+    }
+
+    /**
+     * This method sorted on Twisted-3-7.
+     *
+     * @param array sorted
+     * @return Interger array
+     */
+    public Integer[] sortTwistedThreeSeven(Integer[] array) {
+        return Arrays.stream(array)
+                .map(new SortedOnTwisted()::swapThreeSeven)
+                .sorted(Integer::compare)
+                .map(new SortedOnTwisted()::swapThreeSeven)
+                .toArray(Integer[]::new);
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/german/SortedOnTwistedTest.java
+++ src/test/java/org/fundacionjala/coding/german/SortedOnTwistedTest.java
@@ -1,7 +1,72 @@
 package org.fundacionjala.coding.german;
 
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 9/15/2017.
  */
 public class SortedOnTwistedTest {
+    private SortedOnTwisted sortedOnTwisted;
+
+    /**
+     * This method execute before any test.
+     */
+    @Before
+    public void setUp() {
+        sortedOnTwisted = new SortedOnTwisted();
+    }
+
+    /**
+     * Verify sortTwistedThreeSeven.
+     */
+    @Test
+    public void testSortTwistedThreeSevenMethod() {
+        final String actualResult = Arrays.toString(
+                sortedOnTwisted.sortTwistedThreeSeven(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+        final String expectedResult = Arrays.toString(new Integer[]{1, 2, 7, 4, 5, 6, 3, 8, 9});
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Verify sortTwistedThreeSeven.
+     */
+    @Test
+    public void testSortTwistedThreeSevenMethodSecond() {
+        final String actualResult = Arrays.toString(
+                sortedOnTwisted.sortTwistedThreeSeven(new Integer[]{12, 13, 14}));
+        final String expectedResult = Arrays.toString(new Integer[]{12, 14, 13});
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Verify sortTwistedThreeSeven.
+     */
+    @Test
+    public void testSortTwistedThreeSevenMethodThird() {
+        final String actualResult = Arrays.toString(
+                sortedOnTwisted.sortTwistedThreeSeven(new Integer[]{9, 2, 4, 7, 3}));
+        final String expectedResult = Arrays.toString(new Integer[]{2, 7, 4, 3, 9});
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Verify sortTwistedThreeSeven.
+     */
+    @Test
+    public void testSortTwistedThreeSevenMethodUnchangedArray() {
+        Integer[] array = new Integer[]{9, 2, 4, 7, 3};
+        Integer[] arrayCpy = new Integer[]{9, 2, 4, 7, 3};
+        assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}),
+                Arrays.toString(sortedOnTwisted.sortTwistedThreeSeven(array)));
+        assertEquals("You should not modify the input array!", Arrays.toString(arrayCpy),
+                Arrays.toString(array));
+    }
+
 }
+
