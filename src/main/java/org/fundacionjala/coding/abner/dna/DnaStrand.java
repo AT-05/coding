36732be16ab77ecb54b9ec48779d21311b3dad92
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-09-15 20:12:17 -0400

    Kata 8

--- src/main/java/org/fundacionjala/coding/abner/dna/DnaStrand.java
+++ src/main/java/org/fundacionjala/coding/abner/dna/DnaStrand.java
@@ -1,7 +1,21 @@
 package org.fundacionjala.coding.abner.dna;
 
 /**
- * Created by Administrator on 9/15/2017.
+ * This class DNA.
+ * Created by Abner on 9/15/2017.
  */
 public class DnaStrand {
+
+    /**
+     * This class change character.
+     *
+     * @param dna the old dna.
+     * @return the new dna.
+     */
+    public String makeComplement(String dna) {
+        String aux;
+        dna = dna.replace("T", "W").replace("A", "T").replace("W", "A");
+        dna = dna.replace("C", "W").replace("G", "C").replace("W", "G");
+        return dna;
+    }
 }
