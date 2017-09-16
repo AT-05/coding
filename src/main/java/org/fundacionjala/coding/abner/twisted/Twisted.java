cab012577f1174eeb4ff6da47d2ced95f75dc38c
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2017-09-15 21:00:57 -0400

    Kata 9

--- src/main/java/org/fundacionjala/coding/abner/twisted/Twisted.java
+++ src/main/java/org/fundacionjala/coding/abner/twisted/Twisted.java
@@ -1,7 +1,40 @@
 package org.fundacionjala.coding.abner.twisted;
 
+import java.util.Arrays;
+
 /**
+ * This class twisted.
  * Created by Administrator on 9/15/2017.
  */
 public class Twisted {
+
+
+    /**
+     * This function sort twisted.
+     *
+     * @param integers old value;
+     * @return new twsted value;
+     */
+    public  Integer[] sortTwisted(Integer[] integers) {
+        return Arrays.stream(integers)
+                .map(new Twisted()::remplaseThreeSeven)
+                .sorted(Integer::compare)
+                .map(new Twisted()::remplaseThreeSeven)
+                .toArray(Integer[]::new);
+
+    }
+
+    /**
+     * This function remplase.
+     *
+     * @param integer old integer.
+     * @return new array.
+     */
+    private  Integer remplaseThreeSeven(Integer integer) {
+        return Integer.valueOf(integer.toString()
+                .replace("7", "_")
+                .replace("3", "7")
+                .replace("_", "3")
+        );
+    }
 }
