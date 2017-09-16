d9a1095f9b2cb6ed3580963c6b78154806cd725e
Daniel Caballero <daniel.caballero@fundacio-jala.org>
2017-09-16 15:53:14 -0400

    Adding Kata 4

---
+++ src/main/java/org/fundacionjala/coding/daniel/Kata4.java
@@ -0,0 +1,24 @@
+package org.fundacionjala.coding.daniel;
+
+/**
+ * Created by Administrator on 30/08/17.
+ */
+public class Kata4 {
+
+    public String spinWords(String sentence) {
+        String[] words = sentence.split("[\\s]");
+        StringBuilder spinnedSentence = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            spinnedSentence.append(words[i].length() >= 5 ? spin(words[i]) : words[i]);
+            if (i < words.length - 1) {
+                spinnedSentence.append(" ");
+            }
+        }
+        return spinnedSentence.toString();
+    }
+
+    private String spin(String word) {
+        return new StringBuilder(word).reverse().toString();
+
+    }
+}
