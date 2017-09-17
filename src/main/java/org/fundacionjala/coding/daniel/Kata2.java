5d948bab16adaae99d359e8b945e3aae364c7454
Daniel Caballero <daniel.caballero@fundacio-jala.org>
2017-09-16 22:12:02 -0400

    removed kata2 constructor

--- src/main/java/org/fundacionjala/coding/daniel/Kata2.java
+++ src/main/java/org/fundacionjala/coding/daniel/Kata2.java
@@ -5,18 +5,12 @@ package org.fundacionjala.coding.daniel;
  */
 public class Kata2 {
 
-    /**
-     * Kata2.
-     */
-    protected Kata2() {
-
-    }
 
     /**
      * @param number A positive number.
      * @return Returns the sum of all the multiples of 3 or 5 below the number passed in.
      */
-    protected static int solution(int number) {
+    protected int solution(int number) {
         int sum = 0;
         for (int i = 3; i < number; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
--- src/test/java/org/fundacionjala/coding/daniel/Kata2Test.java
+++ src/test/java/org/fundacionjala/coding/daniel/Kata2Test.java
@@ -8,14 +8,17 @@ import static org.junit.Assert.assertEquals;
  * Created by Administrator on 04/09/17.
  */
 public class Kata2Test {
+
+    private Kata2 kata = new Kata2();
+
     /**
      * Tests for Kata2.
      */
     @Test
     public void test() {
-        assertEquals(23, Kata2.solution(10));
-        assertEquals(78, Kata2.solution(20));
-        assertEquals(9168, Kata2.solution(200));
+        assertEquals(23, kata.solution(10));
+        assertEquals(78, kata.solution(20));
+        assertEquals(9168, kata.solution(200));
     }
 
 }
