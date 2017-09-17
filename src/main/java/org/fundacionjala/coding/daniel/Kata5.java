6a5d1b02028d378bc5672a06d891f72976c16ca7
Daniel Caballero <daniel.caballero@fundacio-jala.org>
2017-09-17 17:10:17 -0400

    fixed kata 5

--- src/main/java/org/fundacionjala/coding/daniel/Kata5.java
+++ src/main/java/org/fundacionjala/coding/daniel/Kata5.java
@@ -10,22 +10,15 @@ import java.util.stream.Collectors;
 class Kata5 {
 
     /**
-     * Constructor kata 5.
-     */
-
-    protected Kata5() {
-
-    }
-
-    /**
      * @param words a sentence or words.
      * @return A sorted inner content of every word
      * of the string in descending order.
      */
-    public static String sortTheInnerContent(String words) {
+    public String sortTheInnerContent(String words) {
         return Arrays.asList(words.split(" ")).stream()
-                .map(word -> word.length() < 3 ? word : word.charAt(0)
-                        + sortChars(word) + word.charAt(word.length() - 1))
+                .map(word -> word.length() < 3 ? word : new StringBuilder()
+                        .append(word.charAt(0)).append(sortChars(word))
+                        .append(word.charAt(word.length() - 1)).toString())
                 .collect(Collectors.joining(" "));
     }
 
@@ -33,7 +26,7 @@ class Kata5 {
      * @param word A string.
      * @return A string sorted.
      */
-    public static String sortChars(String word) {
+    public String sortChars(String word) {
         String[] chars = word.substring(1, word.length() - 1).split("");
         Arrays.sort(chars, Collections.reverseOrder());
         return String.join("", chars);
--- src/test/java/org/fundacionjala/coding/daniel/Kata5Test.java
+++ src/test/java/org/fundacionjala/coding/daniel/Kata5Test.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.coding.daniel;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,14 +9,24 @@ import static org.junit.Assert.assertEquals;
  * Created by Administrator on 15/09/17.
  */
 public class Kata5Test {
+    private Kata5 kata;
+
+    /**
+     * new Kata5.
+     */
+    @Before
+    public void setUp() {
+        kata = new Kata5();
+    }
+
     /**
      * Kata 5 tests.
      */
     @Test
     public void exampleTests() {
         assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
-                Kata5.sortTheInnerContent("sort the inner content in descending order"));
-        assertEquals("wiat for me", Kata5.sortTheInnerContent("wait for me"));
-        assertEquals("tihs ktaa is esay", Kata5.sortTheInnerContent("this kata is easy"));
+                kata.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", kata.sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", kata.sortTheInnerContent("this kata is easy"));
     }
 }
