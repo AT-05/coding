b21b32c1b7f42ba92c2af525f993cbd6b65b9165
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-09-18 14:18:51 +0000

    Adding kata 9 fixed

--- src/main/java/org/fundacionjala/coding/franco/PlanetTwisted.java
+++ src/main/java/org/fundacionjala/coding/franco/PlanetTwisted.java
@@ -7,8 +7,6 @@ import java.util.stream.IntStream;
  * Created by Administrator on 9/14/2017.
  */
 public class PlanetTwisted {
-    private static final int CONS_THREE = 3;
-    private static final int CONS_SEVEN = 7;
 
     /**
      * This method changes all 3 and 7 instances of a set of numbers.
@@ -22,8 +20,8 @@ public class PlanetTwisted {
         equivalent(numbers);
         Arrays.sort(numbers);
         equivalent(numbers);
-        Integer[] result = IntStream.of(numbers).boxed().toArray(Integer[]::new);
-        return result;
+
+        return IntStream.of(numbers).boxed().toArray(Integer[]::new);
     }
 
     /**
@@ -33,9 +31,9 @@ public class PlanetTwisted {
      * @return the equivalent of the number if applies.
      */
     public int replace(int number) {
-        String numbers = String.valueOf(number);
+        final String numbers = String.valueOf(number);
 
-        String result = numbers.replace("7", "B")
+        final String result = numbers.replace("7", "B")
                 .replace("3", "A")
                 .replace("B", "3")
                 .replace("A", "7");
@@ -46,12 +44,10 @@ public class PlanetTwisted {
      * This method replaces equivalents of numbers if apply.
      *
      * @param numbers is a set to work with.
-     * @return the set of numbers with its updated equivalents.
      */
-    public int[] equivalent(int[] numbers) {
+    public void equivalent(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = replace(numbers[i]);
         }
-        return numbers;
     }
 }
