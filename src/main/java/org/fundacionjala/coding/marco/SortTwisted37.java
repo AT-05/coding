80e3c290aee103f1d7e47b60ea4fd5d766655b87
marcomendez <marco.mendez@fundacion-jala.org>
2017-09-19 00:09:28 +0000

    Adding Kata 9

--- src/main/java/org/fundacionjala/coding/marco/SortTwisted37.java
+++ src/main/java/org/fundacionjala/coding/marco/SortTwisted37.java
@@ -1,4 +1,53 @@
 package org.fundacionjala.coding.marco;
 
+import java.util.Arrays;
+
+/**
+ * This was made by Marco Mendez September 18, 2017.
+ */
 public class SortTwisted37 {
+
+    /**
+     * This is the method main.
+     * @param array test.
+     * @return test.
+     */
+    public Integer[] sortTwisted(Integer[] array) {
+        Integer[] arrayNumbers = array.clone();
+        iterateArray(arrayNumbers);
+        Arrays.sort(arrayNumbers);
+        iterateArray(arrayNumbers);
+        return arrayNumbers;
+    }
+
+    /**
+     * This method change if are a 3 for 7 and 7 for 3.
+     *
+     * @param number as a integer.
+     * @return the number as a integer.
+     */
+    public int change3and7(int number) {
+        String num = String.valueOf(number);
+        String[] numString = num.split("");
+        for (int x = 0; x < numString.length; x++) {
+            numString[x] = numString[x].replace("7", "x")
+                    .replace("3", "y")
+                    .replace("x", "3")
+                    .replace("y", "7");
+        }
+        num = String.join("", numString);
+        return Integer.parseInt(num);
+    }
+
+    /**
+     * This method iterate over the array of numbers.
+     *
+     * @param numbers numbers as a integer array.
+     */
+    public void iterateArray(Integer[] numbers) {
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = change3and7(numbers[index]);
+        }
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/marco/SortTwisted37Test.java
+++ src/test/java/org/fundacionjala/coding/marco/SortTwisted37Test.java
@@ -1,4 +1,48 @@
 package org.fundacionjala.coding.marco;
 
-public class SortTwisted37 {
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This was made by Marco Mendez September 18, 2017.
+ */
+public class SortTwisted37Test {
+
+    private SortTwisted37 sortTwisted37;
+
+    /**
+     * This executed before all test.
+     */
+    @Before
+    public void setUp() {
+        sortTwisted37 = new SortTwisted37();
+    }
+
+    /**
+     * Test basic.
+     */
+    @Test
+    public void basicTests() {
+        assertEquals(Arrays.toString(new Integer[]{1, 2, 7, 4, 5, 6, 3, 8, 9}),
+                Arrays.toString(sortTwisted37.sortTwisted(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9})));
+        assertEquals(Arrays.toString(new Integer[]{12, 14, 13}),
+                Arrays.toString(sortTwisted37.sortTwisted((new Integer[]{12, 13, 14}))));
+        assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}),
+                Arrays.toString(sortTwisted37.sortTwisted(new Integer[]{9, 2, 4, 7, 3})));
+    }
+
+    /**
+     * Test array.
+     */
+    @Test
+    public void unChangedArrayTest() {
+        Integer[] array = new Integer[]{9, 2, 4, 7, 3};
+        Integer[] arrayCpy = new Integer[]{9, 2, 4, 7, 3};
+        assertEquals(Arrays.toString(new Integer[]{2, 7, 4, 3, 9}), Arrays.toString(sortTwisted37.sortTwisted(array)));
+        assertEquals("You should not modify the input array!", Arrays.toString(arrayCpy), Arrays.toString(array));
+    }
 }
